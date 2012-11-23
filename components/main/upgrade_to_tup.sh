f94e8375d5e0c195a2ee8892e9ec1617c0c6472b
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-23 16:03:49 +0100

    Write script to tupify components

---
+++ components/main/upgrade_to_tup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+c=`ls|grep -v *.cpp|grep -v *.h|grep -v .sh | grep -v Tupfile |grep -v pythonCodegen`
+
+for x in c
+do
+	(
+	cd $x
+
+	# Switch build system
+	find . -name CMakeLists.txt | xargs git rm
+	# This is a "herefile" that spits out a Tupfile.
+	# This may or may not be cleaner than copying a template Tupfile.
+	(
+	cat <<'EOF'
+include_rules
+: foreach *.cpp |> !cxx |>
+: *.o |> !ld |> %d.so
+EOF
+	)>Tupfile
+	git add Tupfile
+
+	# unROSify the manifest
+	xmlstarlet sel -t -v //description manifest.xml >readme.txt
+	sed -e :a -e '/./,$!d;/^\n*$/{$d;N;};/\n$/ba' readme.txt
+	git add readme.txt
+	git rm manifest*.xml
+
+	# unROSify the scripts that "test" the component
+	git mv $x.ops test.ops
+	git rm run*.sh
+	echo "#!/usr/bin/env bash">test.sh
+	echo "deployer-gnulinux -lerror -s test.ops" >> test.sh
+	chmod +x test.sh
+	git add test.sh
+
+	# Flatten the directory structure a bit
+	git mv src/* .
+
+	)
+done
