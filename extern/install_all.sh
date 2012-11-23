551ea902fcb74c7f046e5ea2e26663d19565dfc6
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-23 12:17:25 +0100

    Add tup as submodule

--- extern/install_all.sh
+++ extern/install_all.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 (
+cd tup
+if [ -x `which tup`]
+	tup upd .
+else
+	./bootstrap.sh
+fi
+sudo cp tup /usr/local/bin/
+sudo cp tup.1 /usr/local/share/man/man1/
+)
+
+(
 cd rtt
 cmake . -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB
 make -j4
@@ -18,14 +29,4 @@ cmake .
 make -j4
 sudo make install
 )
-
-## Set up the run-time linker to find these libs in place.
-#(
-#sudo su
-#SUBDIRS=`find . -name *.so | xargs -L1 dirname | sort | uniq | xargs -L1 readlink -e`
-#for x in $SUBIDRS
-#do
-	#ldconfig $x
-#done
-#)
 
