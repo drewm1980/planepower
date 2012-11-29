c9c1fd5038c6396d0d446f8c5cc1d94d1c70a2e6
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 20:27:38 +0100

    debugging upgrade_to_tup

--- components/upgrade_to_tup.sh
+++ components/upgrade_to_tup.sh
@@ -5,7 +5,7 @@ c=`find lowlevel -name manifest.xml | xargs -n1 dirname | xargs -n1 basename | g
 
 for x in $c
 do
-	./upgrade_component_to_tup.sh $x
+	./upgrade_component_to_tup.sh lowlevel/$x
 done
 
 #c=`ls|grep -v *.cpp|grep -v *.h|grep -v .sh | grep -v Tupfile`
