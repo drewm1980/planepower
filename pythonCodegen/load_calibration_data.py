cad59624c156f44276054384784766d4bc57ee9a
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 19:35:55 +0100

    Fixing broken symlinks in pythonCodegen

--- pythonCodegen/load_calibration_data.py
+++ pythonCodegen/load_calibration_data.py
@@ -3,19 +3,22 @@ import numpy
 from numpy import loadtxt, dot
 from rotation_matrices import invert_se3
 
+cameraspecs='../properties/cameras'
+markersspecs='../properties/markers'
+
 # Intrinsics for camera 1
-PdatC1 = loadtxt('cameras/PC1.dat') # fx fy cx cy
+PdatC1 = loadtxt(cameraspecs+'/PC1.dat') # fx fy cx cy
 f1 = PdatC1[:2]
 c1 = PdatC1[2:]
 
 # Intrinsics for camera 2
-PdatC2 = loadtxt('cameras/PC2.dat') # fx fy cx cy
+PdatC2 = loadtxt(cameraspecs+'/PC2.dat') # fx fy cx cy
 f2 = PdatC2[:2]
 c2 = PdatC2[2:]
 
 # Extrinsics
-RPC1 = loadtxt('cameras/RPC1.dat') # maps points in camera 1 frame into points in arm frame
-RPC2 = loadtxt('cameras/RPC2.dat') # maps points in camera 2 frame into points in arm frame
+RPC1 = loadtxt(cameraspecs+'/RPC1.dat') # maps points in camera 1 frame into points in arm frame
+RPC2 = loadtxt(cameraspecs+'/RPC2.dat') # maps points in camera 2 frame into points in arm frame
 T = dot(invert_se3(RPC1),RPC2)  # Maps points in camera 2 frame into camera 1 frame
 
 RPC1inv = invert_se3(RPC1) # maps points arm frame into camera 1 frame
@@ -23,7 +26,7 @@ RPC2inv = invert_se3(RPC2) # maps points arm frame into camera 2 frame
 Tinv = invert_se3(T) # Maps points in camera 1 frame into camera 2 frame
 
 # Marker locations in the body (attached to plane) frame
-m1_body = loadtxt('./markers/pos_marker_body1.dat')
-m2_body = loadtxt('./markers/pos_marker_body2.dat')
-m3_body = loadtxt('./markers/pos_marker_body3.dat')
+m1_body = loadtxt(markerspecs+'/pos_marker_body1.dat')
+m2_body = loadtxt(markerspecs+'/pos_marker_body2.dat')
+m3_body = loadtxt(markerspecs+'/pos_marker_body3.dat')
 
