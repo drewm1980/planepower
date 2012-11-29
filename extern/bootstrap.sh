aecca91eb0b5940026f9dc4a117434231b5ab029
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 17:51:56 +0100

    Working on bootstrapping acado repos

--- extern/bootstrap.sh
+++ extern/bootstrap.sh
@@ -58,3 +58,21 @@ cmake .
 make_python
 sudo make install_python
 )
+
+echo "Bootstrapping ACADO..."
+(
+cd acado_public
+cmake .
+make -j3
+#DO NOT INSTALL ACADO!!! IT INSTALLS SOME THINGS IN /usr/lib!!!
+)
+(
+cd acado_private
+source ../acado_public/acado_env.sh
+(cd testing/mvukov/mhe_export
+cmake .
+make -j3
+)
+
+# To build, need environment variables from:
+#source testing/mvukov/mhe_export/mhe_export_env.sh
