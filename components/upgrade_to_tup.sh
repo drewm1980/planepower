64ba02ffd40a5f903ddb1ce7db22f73c145b969c
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 20:14:48 +0100

    debugging upgrade_to_tup

--- components/upgrade_to_tup.sh
+++ components/upgrade_to_tup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# List of orocos components
-c=`ls|grep -v *.cpp|grep -v *.h|grep -v .sh | grep -v Tupfile`
-
-c = `find lowlevel -name manifest.xml | xargs -n1 dirname | xargs -n1 basename | grep -v soem`
+# Upgrade the components in lowlevel
+c=`find lowlevel -name manifest.xml | xargs -n1 dirname | xargs -n1 basename | grep -v soem`
 
 for x in $c
 do
 	./upgrade_component_to_tup.sh $x
 done
+
+#c=`ls|grep -v *.cpp|grep -v *.h|grep -v .sh | grep -v Tupfile`
