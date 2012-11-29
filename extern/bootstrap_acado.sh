8a30c97f515cc19b96c4288c6519ca83d4f69ac5
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 19:02:26 +0100

    Updating acado public

--- extern/bootstrap_acado.sh
+++ extern/bootstrap_acado.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+(cd acado_public; git log -1 | grep git-svn-id | cut -d ' ' -f 6)
+(cd acado_private; git log -1 | grep git-svn-id | cut -d ' ' -f 6)
+
 echo "Bootstrapping ACADO..."
 (
 cd acado_public
@@ -7,10 +10,10 @@ cmake .
 make -j3
 #DO NOT INSTALL ACADO!!! IT INSTALLS SOME THINGS IN /usr/lib!!!
 )
+
+source acado_public/acado_env.sh
 (
-cd acado_private
-source ../acado_public/acado_env.sh
-(cd testing/mvukov/mhe_export
+cd acado_private/testing/mvukov/mhe_export
 cmake .
 make -j3
 )
