c41734bb463a7905b49ecc5793d1a23664fad10c
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-11-29 20:21:24 +0100

    debugging upgrade_to_tup

--- components/upgrade_component_to_tup.sh
+++ components/upgrade_component_to_tup.sh
@@ -7,15 +7,18 @@
 #
 # Oh, and it also assumes you're using git like most sane people these days ;)
 
-x = $1
+x=$1
+
 (
 echo "Upgrading component $x..."
 cd $x
 
-# Switch build system
+# Blow away soon to be obsolete .gitignore files
 touch .gitignore
 find . -name ".gitignore" | xargs git rm --ignore-unmatch -q
 rm -f .gitignore
+
+# Replace the CMakeLists file with a simple Tupfile
 find . -name CMakeLists.txt | xargs git rm -q
 # This is a "herefile" that spits out a Tupfile.
 # This may or may not be cleaner than copying a template Tupfile.
@@ -28,13 +31,15 @@ EOF
 )>Tupfile
 git add Tupfile
 
-# unROSify the manifest
+# unROSify manifest.xml
 xmlstarlet sel -t -v //description manifest.xml >readme.txt
 sed -i -e :a -e '/./,$!d;/^\n*$/{$d;N;};/\n$/ba' readme.txt
 git add readme.txt
 git rm manifest*.xml -q
 
-# unROSify the scripts that "test" the component
+# unROSify the scripts that "test" the component.
+# Note: the shebang will only work for you when/if some
+# patches from Andrew Wagner make it into mainline.
 echo "#!/usr/bin/env deployer" > test.ops
 echo "" >> test.ops
 cat $x.ops >> test.ops
@@ -43,10 +48,13 @@ git add test.ops
 git rm $x.ops
 git rm run*.sh -q
 
-#echo "#!/usr/bin/env bash" > test.sh
-#echo "deployer-gnulinux -lerror -s test.ops" >> test.sh
-#chmod +x test.sh
-#git add test.sh
+# Output a test.sh file just in case the deployer
+# does ~not have the above mentioned patch, or
+# the user wants to modify the debug level.
+echo "#!/usr/bin/env bash" > test.sh
+echo "deployer-gnulinux -lerror -s test.ops" >> test.sh
+chmod +x test.sh
+git add test.sh
 
 # Flatten the directory structure a bit
 git mv src/* .
