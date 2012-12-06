aa44c6e7bed5a239eff9ec2bab32f8d6bfdb5338
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-12-06 11:54:06 +0100

    minor upgrade script update

--- components/upgrade_to_tup.sh
+++ components/upgrade_to_tup.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
 # Upgrade the components in lowlevel
-#c=`find lowlevel -name manifest.xml | xargs -n1 dirname | xargs -n1 basename | grep -v soem`
-
 # Building: 
 c="encoder encoderLatch mcuHandler safeStopper segFaulter voltageController"
-
 # Not building yet: soem
 
-for x in $c
-do
-	./upgrade_component_to_tup.sh lowlevel/$x
-done
+for x in $c; do ./upgrade_component_to_tup.sh lowlevel/$x; done
 
 #c=`ls|grep -v *.cpp|grep -v *.h|grep -v .sh | grep -v Tupfile`
+
