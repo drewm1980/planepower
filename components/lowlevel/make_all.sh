340a76cff286b85c0c355c76b0f72bc147f596b4
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-02-20 13:49:06 +0100

    Remove no longer needed build scripts

--- components/lowlevel/make_all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rosmake --build-everything encoder encoderLatch mcuHandler reinsertCameras safeStopper segFaulter soem voltageController
--- components/main/make_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rosmake --build-everything calculateCameraParameters calibrationCamera cameraCalibration cameraSimulator cameraTrigger controlDerivativeHandler controlsPlayback dynamicMHE dynamicMPC imuBuffer imuSimulator LEDTracker LEDTrackerSimulator lqrController lqrController_control_derivatives manualController masterTimer measurementsPlayback measurementsRecorder planeSimulator planeSimulatorDirectControls planeSimulatorRframe planeSimulatorRframeDirectControls playControls poseFromMarkers protobufBridge simpleTrajectoryGenerator winchControl
-
-echo "There are `find . -name manifest.xml | wc -l` manifest.xml's"
-echo "There are `find . -name *-gnulinux.so | wc -l` things that look like build components"
