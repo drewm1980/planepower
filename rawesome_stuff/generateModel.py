7f215b069541b1cf708ac423966543476937a86f
Greg Horn <gregmainland@gmail.com>
2013-03-07 13:34:37 +0100

    update generateModel to use rawesome configuration changes

--- rawesome_stuff/generateModel.py
+++ rawesome_stuff/generateModel.py
@@ -1,18 +1,15 @@
 import rawe
 import casadi as C
-from casadi import *
-import numpy
 
 if __name__=='__main__':
-    print "reading config..."
-    conf = rawe.config.readConfig('config.ini','configspec.ini')
-    
     print "creating model..."
+    from highwind_carousel_conf import conf
     dae = rawe.models.carousel(conf)
     
     modelFile = dae.acadoModelGen()
-    
-    print "saving model..."
-    f = open('out/model.cpp','w')
+
+    filename = 'out/model.cpp'
+    print 'exporting model to "'+filename+'"...'
+    f = open(filename,'w')
     f.write(modelFile)
     f.close()
