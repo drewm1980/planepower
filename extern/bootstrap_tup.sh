f63af726a3e083dc926008522aaa80eb4c96747e
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-03-11 21:09:45 +0100

    Remove tup and orocos_kinematics_dynamics as submodules, update bootstrap_tup.sh

--- extern/bootstrap_tup.sh
+++ extern/bootstrap_tup.sh
@@ -1,15 +1,19 @@
-(
+# This script checks out, builds, and reinstalls tup.
 echo "Bootstrapping tup..."
-cd tup
-if [ -x `which tup` ]
-then
-	echo "Tup installed!"
-	tup upd .
-else
-	echo "Tup not installed!"
-	./bootstrap.sh
+buildloc=~/src/
+mkdir -p $buildloc
+(
+cd $buildloc
+if [ ! -d $buildloc/tup ]; then
+	git clone git://github.com/gittup/tup.git
 fi
-sudo cp tup /usr/local/bin/
+(
+cd tup
+git pull
+./bootstrap.sh
+sudo cp build/tup /usr/local/bin/
 sudo cp tup.1 /usr/local/share/man/man1/
-echo "Done bootstrapping tup"
 )
+)
+echo "Done bootstrapping tup"
+
