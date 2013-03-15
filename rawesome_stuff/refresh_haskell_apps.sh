efe26f23287893ab1f074730c199b5d5091e699f
Greg Horn <gregmainland@gmail.com>
2013-03-15 16:27:10 +0100

    rawesome upstream change for plotter

--- rawesome_stuff/refresh_haskell_apps.sh
+++ rawesome_stuff/refresh_haskell_apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd rawesome && ./mkprotos.sh; cd ..
-cd rawesome/plotter && cabal build; cd ../..
+cd rawesome/plot-ho-matic && cabal build; cd ../..
 cd rawesome/wtfviz && cabal build; cd ../..
 
