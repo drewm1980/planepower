2373263dedc3509619106255ffdb1edc89cba59c
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-03-21 23:49:12 +0100

    Finally building dynamicMHE.so using tup!!!!
    This commit is dedicated to all you wack MHE's.
    
    Seriously though, the final special sauce was recognizing that
    model_constants.hpp needed the CODEGEN_FOR_OROCOS flag set.
    Otherwise, the exported acadoVariables struct does not have
    "p" field and the orocos component fails to compile.
    
    This was with acado private svn version 3179,
    and with acado public svn version 172.

--- extern/bootstrap_acado.sh
+++ extern/bootstrap_acado.sh
@@ -1,22 +1,45 @@
-
 echo "Bootstrapping ACADO..."
 
-echo "Downloading and unpacking a stable acado revision from tar file..."
-ACADO_PRIVATE=acado_private_r3094_trimmed
-if [ ! -f $ACADO_PRIVATE.tar.gz ]; then
-	scp gitmirrorbot@moinette.esat.kuleuven.be:$ACADO_PRIVATE.tgz .
-fi
-[ ! -d $ACADO_PRIVATE ] && tar -xvf $ACADO_PRIVATE.tgz
+#echo "Downloading and unpacking a stable acado revision from tar file..."
+#ACADO_PRIVATE=acado_private_r3094_trimmed
+#if [ ! -f $ACADO_PRIVATE.tar.gz ]; then
+	#scp gitmirrorbot@moinette.esat.kuleuven.be:$ACADO_PRIVATE.tgz .
+#fi
+#[ ! -d $ACADO_PRIVATE ] && tar -xvf $ACADO_PRIVATE.tgz
 
-echo "Make main acado..."
+echo "Cloning acado_public"
+ACADO_PUBLIC=acado_public
+git clone gitmirrorbot@moinette.esat.kuleuven.be:acado_private $ACADO_PUBLIC
 (
-mkdir -p $ACADO_PRIVATE/build
-cd $ACADO_PRIVATE/build
+cd $ACADO_PUBLIC
+git co kurt_at_planepower_during_embocon
+)
+echo "Making acado_public..."
+(
+mkdir -p $ACADO_PUBLIC/build
+cd $ACADO_PUBLIC/build
 cmake ..
 make
 )
 
-source $ACADO_PRIVATE/build/acado_env.sh
+echo "Cloning acado_private"
+ACADO_PRIVATE=acado_private
+git clone gitmirrorbot@moinette.esat.kuleuven.be:acado_private $ACADO_PRIVATE
+git co kurt_at_planepower_during_embocon
+(
+cd $ACADO_PRIVATE
+git co kurt_at_planepower_during_embocon
+)
+#echo "Making acado_private..."
+#(
+#mkdir -p $ACADO_PRIVATE/build
+#cd $ACADO_PRIVATE/build
+#cmake ..
+#make
+#)
+
+source $ACADO_PUBLIC/build/acado_env.sh
+#source $ACADO_PRIVATE/build/acado_env.sh
 
 echo "Make mhe codegen stuff..."
 (
