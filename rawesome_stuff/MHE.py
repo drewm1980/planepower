4557d13f4288c700d5004b14a84c7aa759de903f
Kurt Geebelen <kurt.geebelen@mech.kuleuven.be>
2013-04-04 20:01:34 +0200

    added measurements and options.

--- rawesome_stuff/MHE.py
+++ rawesome_stuff/MHE.py
@@ -7,20 +7,79 @@ if __name__=='__main__':
 
     from rawe.ocp import Ocp
     mhe = Ocp(dae, N=10, ts=0.1)
-    mhe.constrain( mpc['motor_torque'], '==', 0 );
-    mhe.constrain( mpc['ddr'], '==', 0 );
-    mhe.constrain( -32767/1.25e6, '<=', mpc['aileron'] );
-    mhe.constrain( mpc['aileron'], '<=', 32767/1.25e6 );
-    mhe.constrain( -32767/2e5, '<=', mpc['elevator'] );
-    mhe.constrain( mpc['elevator'], '<=', 32767/2e5 );
-    mhe.constrain( -0.2, '<=', mpc['daileron'] );
-    mhe.constrain( mpc['daileron'], '<=', 0.2 );
-    mhe.constrain( -1, '<=', mpc['delevator'] );
-    mhe.constrain( mpc['delevator'], '<=', 1 );
 
-    measurements = C.veccat( [dae[n] for n in ['marker positions','IMU acceleration','IMU angular velocity','delta']])
+    measurements = C.veccat( [dae[n] for n in ['marker positions','IMU acceleration','IMU angular velocity']])
+    measurements = C.veccat( [measurements,
+                              dae['cos_delta'],
+                              dae['aileron'],
+                              dae['elevator'],
+                              dae['motor_torque'],
+                              dae['daileron'],
+                              dae['delevator']])
 
     mhe.minimizeLsq(measurements)
     mhe.minimizeLsqEndTerm(measurements)
+    e11 = dae['e11']
+    e12 = dae['e12']
+    e13 = dae['e13']
 
-    #mhe.exportCode(CXX='clang++')
+    e21 = dae['e21']
+    e22 = dae['e22']
+    e23 = dae['e23']
+
+    e31 = dae['e31']
+    e32 = dae['e32']
+    e33 = dae['e33']
+
+    x = dae['x']
+    y = dae['y']
+    z = dae['z']
+
+    dx = dae['dx']
+    dy = dae['dy']
+    dz = dae['dz']
+
+    r = dae['r']
+
+    ConstR1 = e11*e11 + e12*e12 + e13*e13 - 1
+    ConstR2 = e11*e21 + e12*e22 + e13*e23
+    ConstR3 = e11*e31 + e12*e32 + e13*e33
+    ConstR4 = e21*e21 + e22*e22 + e23*e23 - 1
+    ConstR5 = e21*e31 + e22*e32 + e23*e33
+    ConstR6 = e31*e31 + e32*e32 + e33*e33 - 1
+
+    Const = - r*r/2 + x*x/2 + y*y/2 + z*z/2
+    dConst = dx*x + dy*y + dz*z
+
+    mhe.constrain(ConstR1,'==',0)
+    mhe.constrain(ConstR2,'==',0)
+    mhe.constrain(ConstR3,'==',0)
+    mhe.constrain(ConstR4,'==',0)
+    mhe.constrain(ConstR5,'==',0)
+    mhe.constrain(ConstR6,'==',0)
+
+    mhe.constrain(Const,'==',0)
+    mhe.constrain(dConst,'==',0)
+
+    myoptions = [('HESSIAN_APPROXIMATION','GAUSS_NEWTON'),
+                 ('DISCRETIZATION_TYPE','MULTIPLE_SHOOTING'),
+                 ('HESSIAN_APPROXIMATION','GAUSS_NEWTON'),
+                 ('DISCRETIZATION_TYPE','MULTIPLE_SHOOTING'),
+                 ('INTEGRATOR_TYPE','INT_IRK_GL2'),
+                 ('NUM_INTEGRATOR_STEPS','30'),
+                 ('IMPLICIT_INTEGRATOR_NUM_ITS','3'),
+                 ('IMPLICIT_INTEGRATOR_NUM_ITS_INIT','0'),
+                 ('LINEAR_ALGEBRA_SOLVER','HOUSEHOLDER_QR'),
+                 ('UNROLL_LINEAR_SOLVER','NO'),
+                 ('IMPLICIT_INTEGRATOR_MODE','IFTR'),
+                 ('SPARSE_QP_SOLUTION','CONDENSING'),
+                 ('QP_SOLVER','QP_QPOASES'),
+                 ('HOTSTART_QP','YES'),
+                 ('GENERATE_TEST_FILE','YES'),
+                 ('CG_USE_C99','YES'),
+                 ('PRINTLEVEL','HIGH'),
+                 ('CG_USE_VARIABLE_WEIGHTING_MATRIX','YES')]
+
+
+
+    #mhe.exportCode(CXX='clang++',options=myoptions)
