78089937ade3abae995bc9b6b0433c7a0a1580a5
Greg Horn <gregmainland@gmail.com>
2013-04-10 14:17:52 +0200

    NMPC now works properly (options were set the old way)

--- rawesome_stuff/NMPC.py
+++ rawesome_stuff/NMPC.py
@@ -6,7 +6,8 @@ if __name__=='__main__':
     dae = rawe.models.carousel(conf)
 
     from rawe.ocp import Ocp
-    mpc = Ocp(dae, N=10, ts=0.1)
+    N = 10
+    mpc = Ocp(dae, N=N, ts=0.1)
     mpc.constrain( mpc['motor_torque'], '==', 0 );
     mpc.constrain( mpc['ddr'], '==', 0 );
     mpc.constrain( -32767/1.25e6, '<=', mpc['aileron'] );
@@ -20,19 +21,19 @@ if __name__=='__main__':
 
     ref = C.veccat( [dae[n] for n in ['x','y','z','dx','dy','dz','e11', 'e12', 'e13','e21', 'e22', 'e23','e31', 'e32', 'e33','w1','w2','w3']])
 
-    myoptions=[('HESSIAN_APPROXIMATION','GAUSS_NEWTON'),
+    acadoOpts=[('HESSIAN_APPROXIMATION','GAUSS_NEWTON'),
                ('DISCRETIZATION_TYPE','MULTIPLE_SHOOTING'),
                ('QP_SOLVER','QP_QPOASES'),
                ('HOTSTART_QP','NO'),
                ('INTEGRATOR_TYPE','INT_IRK_GL2'),
-               ('NUM_INTEGRATOR_STEPS','40'),
+               ('NUM_INTEGRATOR_STEPS',str(4*N)),
                ('IMPLICIT_INTEGRATOR_NUM_ITS','3'),
                ('IMPLICIT_INTEGRATOR_NUM_ITS_INIT','0'),
                ('LINEAR_ALGEBRA_SOLVER','HOUSEHOLDER_QR'),
                ('UNROLL_LINEAR_SOLVER','NO'),
                ('IMPLICIT_INTEGRATOR_MODE','IFTR'),
                ('SPARSE_QP_SOLUTION','CONDENSING'),
-               ('AX_NUM_QP_ITERATIONS','30'),
+#               ('AX_NUM_QP_ITERATIONS','30'),
                ('GENERATE_TEST_FILE','YES'),
                ('GENERATE_SIMULINK_INTERFACE','YES'),
                ('GENERATE_MAKE_FILE','NO'),
@@ -41,8 +42,8 @@ if __name__=='__main__':
     mpc.minimizeLsq(ref)
     mpc.minimizeLsqEndTerm(ref)
 
-    options = {'CXX':'g++', 'CC':'gcc'}
-    mpcRT = mpc.exportCode(options,qpSolver='QP_OASES')
+    cgOpts = {'CXX':'g++', 'CC':'gcc'}
+    mpcRT = mpc.exportCode(cgOptions=cgOpts,acadoOptions=acadoOpts,qpSolver='QP_OASES')
 
 
    
