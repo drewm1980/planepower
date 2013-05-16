a08b713983aaa299fc554f8b624bb94a4c365e19
Greg Horn <gregmainland@gmail.com>
2013-05-16 17:54:19 +0200

    setup invariant constraints at top level, not in MHE

--- rawesome_stuff/MHE.py
+++ rawesome_stuff/MHE.py
@@ -27,54 +27,17 @@ def makeMhe(dae,N,dt,nSteps,iType):
     mhe.minimizeLsq(C.veccat([xref,uref]))
     mhe.minimizeLsqEndTerm(xref)
     
-    e11 = dae['e11']
-    e12 = dae['e12']
-    e13 = dae['e13']
+#    mhe.constrain(mhe['ConstR1'],'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstR2'],'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstR3'],'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstR4'],'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstR5'],'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstR6'],'==',0, when='AT_END')
 
-    e21 = dae['e21']
-    e22 = dae['e22']
-    e23 = dae['e23']
+    mhe.constrain(mhe['Const'],'==',0, when='AT_END')
+    mhe.constrain(mhe['dConst'],'==',0, when='AT_END')
 
-    e31 = dae['e31']
-    e32 = dae['e32']
-    e33 = dae['e33']
-
-    x = dae['x']
-    y = dae['y']
-    z = dae['z']
-
-    dx = dae['dx']
-    dy = dae['dy']
-    dz = dae['dz']
-
-    r = dae['r']
-
-    cos_delta = dae['cos_delta']
-    sin_delta = dae['sin_delta']
-    
-    ConstR1 = e11*e11 + e12*e12 + e13*e13 - 1
-    ConstR2 = e11*e21 + e12*e22 + e13*e23
-    ConstR3 = e11*e31 + e12*e32 + e13*e33
-    ConstR4 = e21*e21 + e22*e22 + e23*e23 - 1
-    ConstR5 = e21*e31 + e22*e32 + e23*e33
-    ConstR6 = e31*e31 + e32*e32 + e33*e33 - 1
-
-    Const = - r*r/2 + x*x/2 + y*y/2 + z*z/2
-    dConst = dx*x + dy*y + dz*z
-
-    ConstDelta = cos_delta**2 + sin_delta**2 - 1
-
-#    mhe.constrain(ConstR1,'==',0, when='AT_END')
-#    mhe.constrain(ConstR2,'==',0, when='AT_END')
-#    mhe.constrain(ConstR3,'==',0, when='AT_END')
-#    mhe.constrain(ConstR4,'==',0, when='AT_END')
-#    mhe.constrain(ConstR5,'==',0, when='AT_END')
-#    mhe.constrain(ConstR6,'==',0, when='AT_END')
-
-    mhe.constrain(Const,'==',0, when='AT_END')
-    mhe.constrain(dConst,'==',0, when='AT_END')
-
-#    mhe.constrain(ConstDelta,'==',0, when='AT_END')
+#    mhe.constrain(mhe['ConstDelta'],'==',0, when='AT_END')
 
     acadoOpts = [('HESSIAN_APPROXIMATION','GAUSS_NEWTON'),
                  ('DISCRETIZATION_TYPE','MULTIPLE_SHOOTING'),
