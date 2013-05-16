1c3a476af71238fb09749a55c0c4a1ca76fefec0
Milan Vukov <mvukov@gmail.com>
2013-05-16 19:14:57 +0200

    Refactored encoder component

--- components/lowlevel/encoder/encoder.cpp
+++ components/lowlevel/encoder/encoder.cpp
@@ -1,171 +1,150 @@
 #include "encoder.hpp"
-#include <ocl/Component.hpp>
 
-ORO_CREATE_COMPONENT( OCL::Encoder)
+#include <rtt/Logger.hpp>
+#include <rtt/Property.hpp>
+#include <rtt/os/TimeService.hpp>
+#include <rtt/Time.hpp>
+
+#include <cmath>
 
 using namespace std;
 using namespace RTT;
-using namespace Orocos;
-using namespace BFL;
+using namespace RTT::os;
 
-// Gear ratio of the gearbox of the carousel
+/// Gear ratio of the gearbox of the carousel
 #define GEAR_RATIO 32.8
-// Number of pulses per revolution of the encoder
+/// Number of pulses per revolution of the encoder
 #define PULSES_PER_REVOLUTION 4096.0
-// Encoder will overflow at this value
-#define ENCODER_OVERFLOW 2147483648.0
-// Encoder threshold to detect overflow
-#define ENCODER_TRESHOLD ENCODER_OVERFLOW/10.0
-// Pi
-#define PI 3.14159265358979323846264338327950288419716939937510
 
-namespace OCL
+Encoder::Encoder(std::string name)
+	: TaskContext( name )
 {
-     Encoder::Encoder(std::string name)
-         : TaskContext(name)
-     {
-        // Add properties
-
-	addProperty("encoderPort",encoderPort);
-	addProperty("extra_ticks",extra_ticks);
-
+	//
 	// Add ports
-
-        addPort( "delta",_delta ).doc("The position of the carousel");
-        addPort( "omega",_omega ).doc("The rotation speed of the carousel");
-	addEventPort("encoderTimeStampIn",_encoderTimeStampIn).doc("time stamp of the encoder signal");
-	addPort("encoderTimeStampOut",_encoderTimeStampOut).doc("time stamp of the encoder signal");
-	addPort("computationTime",_computationTime);
-	addPort("deltaOmega",_deltaOmega);
-
-	deltaOmega.resize(2,0.0);
-	encoderTimeStamp = RTT::os::TimeService::Instance()->getTicks(); // Get current time
-
-	_delta.setDataSample( 0.0 );
-	_delta.write( 0.0 );
-	_omega.setDataSample( 0.0 );
-	_omega.write( 0.0 );
-	_encoderTimeStampOut.setDataSample( encoderTimeStamp );
-	_encoderTimeStampOut.write( encoderTimeStamp );
-	_computationTime.setDataSample( encoderTimeStamp );
-	_computationTime.write( encoderTimeStamp );
-	_deltaOmega.setDataSample( deltaOmega );
-	_deltaOmega.write( deltaOmega );
-    }
-    
-
-    Encoder::~Encoder()
-    {
-    }
-
-    bool  Encoder::configureHook()
-    {
-	assert(this->hasPeer("soemMaster"));
-	assert(this->getPeer("soemMaster")->provides()->hasService("Slave_1001"));
-	assert(this->getPeer("soemMaster")->provides()->getService("Slave_1001")->provides()->hasOperation("readEncoder"));
-	readEncoder = getPeer("soemMaster")->provides()->getService("Slave_1001")->provides()->getOperation("readEncoder");
-        return true;
-     }
-
-    bool  Encoder::startHook()
-    {
-	for (int i=0;i<REGSIZE;i++) diffcount[i]=0;
-	for (int i=0;i<REGSIZE;i++)  difftime[i]=0;
-	regi = 0;
-	mycount_new = readEncoder(encoderPort) ; // Read once, because the first time it seems to fail and give back some random value
-	mycount_new=0;
-	mycount_old=0;
-	tickRegister = RTT::os::TimeService::Instance()->getTicks(); // Get current time and store it in the register
-	diff = 0.0;
-	extra_ticks = 0.0;
-	overflowup = false;
-	overflowdown = false;
-        return true;
-    }
-
-    void  Encoder::updateHook()
-    {
-RTT::os::TimeService::ticks tic = RTT::os::TimeService::Instance()->getTicks();
-	mycount_new = readEncoder(encoderPort) ;
-
-#if 0
-	SendHandle<int(unsigned int)> encoderHandle = readEncoder.send(encoderPort);
-
-	// collect takes the return value of getName() as first argument and fills it in:
-	SendStatus ss = encoderHandle.collect(mycount_new); // blocks until operation is complete
-	if (!(ss == SendSuccess)) {
-		//cout << "mycount_new was: " << mycount_new << endl; 
-	}
-#endif
-	_computationTime.write((TIME_TYPE) RTT::os::TimeService::Instance()->getTicks(tic) );
-	_encoderTimeStampIn.read(encoderTimeStamp);
-
+	//
+	addEventPort("trigger", portTrigger)
+		.doc("Trigger port for the component.");
+	addPort("encoderData", portEncoderData)
+		.doc(	"Output port for encoder data:"
+				"[timestamp, delta, sin_delta, cos_delta, omega, omega_filtered, omega_rpm].");
+	addPort("execTime", portExecTime)
+		.doc("Execution time of the component.");
 	
-	// First calculate the time passed from previous updateHook
-	myticks = RTT::os::TimeService::Instance()->getTicks(); // Get current time
-	deltansec = RTT::os::TimeService::ticks2nsecs(myticks-tickRegister); // Calculate the difference with previous time (in nanoseconds)
-	myperiod = deltansec/1.0e9; // Convert to seconds
-	if (myperiod==0.0) return;
-	tickRegister=myticks; // Store time to be used in next updateHook
-	// Now calculate the difference in encoder ticks
-	diff=mycount_new-mycount_old; // Calculate the difference with previous encoder ticks
-	// Try to catch if the encoder has encountered an overflow
-	if(abs(diff>ENCODER_TRESHOLD)){ //Overflow!!
-		// Ok, so we have an overflow. Now detect in which direction, and make it correct.
-//		cout << "Encoder overflow detected" << endl;
-//cout << "mycount_old " << mycount_old << endl;
-//cout << "mycount_new " << mycount_new << endl;
-		if(mycount_old > 0){diff+=ENCODER_OVERFLOW; overflowup = true;}
-		if(mycount_old < 0){diff-=ENCODER_OVERFLOW; overflowdown=true;}
+	//
+	// Add properties
+	//
+	addProperty("encoderPort", encoderPort);
+
+	//
+	// Prepare ports
+	//
+	encoderData.resize(7, 0.0);
+	portEncoderData.setDataSample( encoderData );
+	portEncoderData.write( encoderData );
+}
+
+bool  Encoder::configureHook()
+{
+	if (this->hasPeer("soemMaster") == false)
+	{
+		log( Error ) << "There is no peer" << "soemMaster" << endlog();
+		goto configureHookFail;
 	}
-	mycount_old=mycount_new; // And store previous encoder ticks
-
-	// And calculate the velocity
-	diffcount[regi] = diff;
-	difftime[regi] = myperiod;
-	regi=(regi+1) % REGSIZE;
-
-	//Average out over [REGSIZE] measurements
-	omega=0;
-	for (int i=0;i<REGSIZE;i++){
-		omega += (double) diffcount[i]/difftime[i];
+	
+	if (this->getPeer("soemMaster")->provides()->hasService("Slave_1001") == false)
+	{
+		log( Error ) << "There is no service" << "Slave_1001" << endlog();
+		goto configureHookFail;
 	}
-	omega /= REGSIZE;
-
-	omega*=2*PI/GEAR_RATIO/PULSES_PER_REVOLUTION; //Convert to correct dimensions (rad/s)
-
-	// Now calculate the angle of the carousel
-
-	// First, account for overflow situations
-	if(overflowup){
-		extra_ticks += ENCODER_OVERFLOW; // This many ticks will be ignored, so we add them
-		extra_ticks = fmod(extra_ticks,GEAR_RATIO*PULSES_PER_REVOLUTION); // To be sure that extra_ticks doesn't become too big (probably never happens, or after 4 years of running, but better safe than sorry :-) )
-		overflowup=false; // Put the flag back to false, because it isn't true anymore (or we have to prove otherwise in next update)
+	
+	if (this->getPeer("soemMaster")->provides()
+		->getService("Slave_1001")->provides()->hasOperation("readEncoder") == false)
+	{
+		log( Error ) << "There is no operation" << "readEncoder" << endlog();
+		goto configureHookFail;
 	}
-	if(overflowdown){
-		extra_ticks -= ENCODER_OVERFLOW; // This many ticks will be ignored, so we add them
-		extra_ticks = fmod(extra_ticks,GEAR_RATIO*PULSES_PER_REVOLUTION); // To be sure that extra_ticks doesn't become too big (probably never happens, or after 4 years of running, but better safe than sorry :-) )
-		overflowdown=false; // Put the flag back to false, because it isn't true anymore (or we have to prove otherwise in next update)
-	}
-	// Now calculate the angle
-	delta = 2.0*PI*( (double) mycount_new + extra_ticks)/(GEAR_RATIO*PULSES_PER_REVOLUTION);
-	// And put it on the ports
-	deltaOmega[0] = -delta;
-	deltaOmega[1] = -omega;
-	_encoderTimeStampOut.write(encoderTimeStamp);
-	_delta.write(-delta); // with a minus sign because it is defined in counter clock wise direction
-	_omega.write(-omega); // Put it on the port. With minus sign, because rotation should be positive for counter clock wise rotation
-	_deltaOmega.write(deltaOmega);
-    }
+	
+	readEncoder = getPeer("soemMaster")->provides()
+		->getService("Slave_1001")->provides()->getOperation("readEncoder");
+		
+	return true;
+	
+	configureHookFail:
+	
+	return false;
+}
 
-    void  Encoder::stopHook()
-    {
-    }
+bool  Encoder::startHook()
+{
+	posOld = readEncoder( encoderPort );
+	omegaOld = 0.0;
+	timeStampOld = TimeService::Instance()->getTicks();
+	posAcc = 0.0;
+	
+    return true;
+}
 
-    void  Encoder::cleanUpHook()
-    {
-    }
+void  Encoder::updateHook()
+{
+	TimeService::ticks tickStart = TimeService::Instance()->getTicks();
 
+	// Read time stamp
+	portTrigger.read( triggerTimeStamp );
+	encoderData[ 0 ] = (double)triggerTimeStamp;
+	
+	// Read new position and corresponding time-stamp
+	posNew = readEncoder( encoderPort );
+	timeStampNew = TimeService::Instance()->getTicks();
+	
+	// Read elapsed time since the last position reading
+	elapsedTime = TimeService::Instance()->secondsSince( timeStampOld );
+	
+	// Assuming that posOld, posNew and posDelta are 32bit signed numbers
+	// (int's) there are no sign nor overflow problems.
+	int posDelta = posNew - posOld;
+	
+	// Convert encoder ticks to real angle in radians and bound it to -pi.. pi
+	double posDeltaReal = (double)posDelta * 2.0 * M_PI / (GEAR_RATIO * PULSES_PER_REVOLUTION);
+	posAcc += posDeltaReal;
+	if (posAcc > M_PI)
+		posAcc -= 2.0 * M_PI;
+	else if (posAcc < M_PI)
+		posAcc += 2.0 * M_PI;
+	
+	// Calculate angular velocity [rad/s]
+	omegaNew = posDeltaReal / elapsedTime;
+	
+	// TODO Apply first order filter to omegaNew
+	
+	// Invert the signs and fill in the output vector
+	encoderData[ 1 ] = -posAcc;
+	encoderData[ 2 ] = sin( -posAcc );
+	encoderData[ 3 ] = cos( -posAcc );
+	encoderData[ 4 ] = -omegaNew;
+	encoderData[ 5 ] = -omegaNew; 
+	encoderData[ 6 ] = -omegaNew * 2 * M_PI / 60.0;
+	
+	// Output data to the port
+	portEncoderData.write( encoderData );
+	
+	// Prepare for the next sampling time.
+	posOld = posNew;
+	timeStampOld = timeStampNew;
+	omegaOld = omegaNew;
+	
+	// Output execution time of the component
+	portExecTime.write(
+		TimeService::Instance()->secondsSince( tickStart )
+	);
+}
 
-}//namespace
+void  Encoder::stopHook()
+{}
 
+void  Encoder::errorHook()
+{}
+
+void  Encoder::cleanupHook()
+{}
+
+ORO_CREATE_COMPONENT( Encoder )
