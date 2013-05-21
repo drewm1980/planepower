1a1e4bff3075c2bc9e118749f47e97200c640ce6
Mario Zanon <mario.zanon@esat.kuleuven.be>
2013-05-21 18:49:44 +0200

    invariant stabilization added, MPC weight definition improved

--- rawesome_stuff/mpc_mhe_simulation.py
+++ rawesome_stuff/mpc_mhe_simulation.py
@@ -14,7 +14,10 @@ from mpc_mhe_utils import *
 from highwind_carousel_conf import conf
 import carouselModel
 
+conf['stabilize_invariants'] = False
 dae = carouselModel.makeModel(conf)
+conf['stabilize_invariants'] = True
+daeSim = carouselModel.makeModel(conf)
 
 measNames  = ['marker_positions','IMU_angular_velocity','IMU_acceleration']
 measNames += ['r','cos_delta','sin_delta','aileron','elevator']
@@ -33,6 +36,40 @@ Covariance = {'marker_positions':1e3,
               'motor_torque':20.,
               'ddr':1e-4}
 
+# Define the weights
+Wp  = 1e-1
+Wdp = 1e-1
+We  = 1.
+Ww  = 1.
+Wr  = 1.
+Wdr  = Wr
+Wdelta = 1e2
+Wddelta = Wdelta
+Wae = 1e2
+Wddr = 1.
+Wmt = 1e-1
+
+Wdmt = 1.
+Wdddr = 1.
+Wdae = 1.
+
+MPCweights = {}
+for name in ['x','y','z']: MPCweights[name] = Wp
+for name in ['dx','dy','dz']: MPCweights[name] = Wdp
+for name in ['e11', 'e12', 'e13', 'e21', 'e22', 'e23', 'e31', 'e32', 'e33']: MPCweights[name] = We
+for name in ['w1','w2','w3']: MPCweights[name] = Ww
+MPCweights['r'] = Wr
+MPCweights['dr'] = Wdr
+MPCweights['ddr'] = Wddr
+MPCweights['cos_delta'] = MPCweights['sin_delta'] = Wdelta
+MPCweights['ddelta'] = Wddelta
+MPCweights['motor_torque'] = Wmt
+MPCweights['aileron'] = MPCweights['elevator'] = Wae
+
+MPCweights['dmotor_torque'] = Wdmt
+MPCweights['dddr'] = Wdddr
+MPCweights['daileron'] = MPCweights['delevator'] = Wdae
+
 measNames = dae.xNames() + dae.uNames()
 endMeasNames = dae.xNames()
 for name in measNames:
@@ -45,14 +82,14 @@ Ts = 0.1    # Sampling time
 nSteps = 20 #Number of steps for the Rintegrator (also in MPC and MHE)
 iType = 'INT_IRK_GL2' # Rintegrator type
 iType = 'INT_IRK_RIIA3' # Rintegrator type
-Tf = 6.   # Simulation duration
+Tf = 10.0   # Simulation duration
 
 # Create the MPC class
 mpcRT, intOpts = makeNmpc(dae,N=N_mpc,dt=Ts,nSteps=nSteps,iType=iType)
 mheRT = makeMhe(dae,N=N_mpc,Ts=Ts,nSteps=nSteps,iType=iType,measNames=measNames,endMeasNames=endMeasNames)
 
 # Create a simulation class
-sim = InitializeSim(dae,'Idas',Ts,intOpts)
+sim = InitializeSim(daeSim,'Idas',Ts,intOpts)
 #sim, simLog = InitializeSim(dae,'RtIntegrator',Ts,intOpts)
 
 # Generate a Rintegrator for linearizing the system
@@ -64,7 +101,7 @@ refP = {'r0':1.2,
         'ddelta0':2*np.pi,
         'z0':-0.1}
             
-InitializeMPC(mpcRT,Rint,dae,conf,refP)
+InitializeMPC(mpcRT,Rint,dae,conf,refP,MPCweights)
 InitializeMHE(mheRT,Rint,dae,conf,refP,Covariance)
 
 new_out = sim.getOutputs(mpcRT.x[0,:],mpcRT.u[0,:],{})
@@ -103,7 +140,9 @@ plotter.subplot([['c'],['cdot']],what=['sim','mhe'])
 plotter.subplot([['_kkt'],['_objective'],['_prep_time','_fb_time']],what=['mpc','mhe'])
 plotter.plot(['_kkt'],what=['mpc','mhe'])
 plotter.plot(['ConstR1','ConstR2','ConstR3','ConstR4','ConstR5','ConstR6'],what=['sim','mhe'])
+plotter.plot(['ConstR1','ConstR2','ConstR3','ConstR4','ConstR5','ConstR6'],what=['sim'])
 plotter.subplot([['c'],['cdot'],['ConstDelta']],what=['sim','mhe'])
+plotter.subplot([['c'],['cdot'],['ConstDelta']],what=['sim'])
 
 #mpcrt.plot(['x','v'],when='all')
 
--- rawesome_stuff/mpc_mhe_utils.py
+++ rawesome_stuff/mpc_mhe_utils.py
@@ -126,10 +126,12 @@ def GenerateReference(dae,conf,refP):
     
     return xref, uref
 
-def InitializeMPC(mpcrt,integrator,dae,conf,refP):
+def InitializeMPC(mpcrt,integrator,dae,conf,refP,MPCweights):
         
     xref, uref = GenerateReference(dae,conf,refP)
-    print xref
+#    xref['x'] += 0.1
+#    xref['e11'] += 0.01
+    
     N = mpcrt.u.shape[0]
     Xref = [xref[name] for name in dae.xNames()]
     Uref = [uref[name] for name in dae.uNames()]
@@ -139,6 +141,7 @@ def InitializeMPC(mpcrt,integrator,dae,conf,refP):
     ts = mpcrt._ts
     # The only part that changes is cos(delta), sin(delta) 
     CS = np.array([ [np.cos(k*ts*refP['ddelta0']), np.sin(k*ts*refP['ddelta0'])] for k in range(N+1) ])
+#    CS[0,0] += 0.1
     for k,name in enumerate(dae.xNames()):
         if name == 'cos_delta': Xref[:,k] = CS[:,0]
         if name == 'sin_delta': Xref[:,k] = CS[:,1]
@@ -159,21 +162,17 @@ def InitializeMPC(mpcrt,integrator,dae,conf,refP):
     # Set the initial state
     mpcrt.x0 = Xref[0,:]
     
-    # Define the weights
-    Wp  = 25.
-    Wdp = 25.
-    We  = 10.
-    Ww  = 0.1
-    Wr  = 100.
-    Wdelta = 100.
-    Wae = 250.
     
-    Q = [Wp]*3 + [Wdp]*3 + [We]*9 + [Ww]*3 + [Wr]*2 + [Wdelta]*3 + [Wae]*2
-    R = [1.0*1e6]*4
+    Q = []
+    R = []
+    for name in mpcrt._dae.xNames():
+        Q.append(MPCweights[name])
+    for name in mpcrt._dae.uNames():
+        R.append(MPCweights[name])
        
-    mpcrt.S = np.diag( Q + R )*1e-2
-    Q = np.diag( Q )*1e-2
-    R = np.diag( R )*1e-2
+    mpcrt.S = np.diag( Q + R )
+    Q = np.diag( Q )
+    R = np.diag( R )
     
     mpcrt.Q = Q
     mpcrt.R = R
@@ -217,6 +216,8 @@ def InitializeMPC(mpcrt,integrator,dae,conf,refP):
 def InitializeMHE(mhert,integrator,dae,conf,refP,Covariance):
     
     xref, uref = GenerateReference(dae,conf,refP)
+#    xref['x'] += 0.1
+#    xref['e11'] += 0.01
     
     N = mhert.u.shape[0]
     Xref = [xref[name] for name in dae.xNames()]
@@ -227,6 +228,7 @@ def InitializeMHE(mhert,integrator,dae,conf,refP,Covariance):
     ts = mhert._ts
     # The only part that changes is cos(delta), sin(delta) 
     CS = np.array([ [np.cos(k*ts*refP['ddelta0']), np.sin(k*ts*refP['ddelta0'])] for k in range(-N,1) ])
+#    CS[0,0] += 0.1
     for k,name in enumerate(dae.xNames()):
         if name == 'cos_delta': Xref[:,k] = CS[:,0]
         if name == 'sin_delta': Xref[:,k] = CS[:,1]
