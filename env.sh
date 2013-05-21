d973d0fc0d649570b06743724160d06504b932d7
Milan Vukov <milan.vukov@esat.kuleuven.be>
2013-05-21 16:39:38 +0200

    Updated .gitignore and main env script

--- env.sh
+++ env.sh
@@ -43,4 +43,4 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PLANEPOWER_ROOT/extern/acado/build/expe
 # instead of the stock version.
 #export PATH=$PLANEPOWER_ROOT/tools:$PATH
 
-source $PLANEPOWER_ROOT/.eprompt
+# source $PLANEPOWER_ROOT/.eprompt
