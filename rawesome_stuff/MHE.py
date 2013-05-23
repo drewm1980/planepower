ac9764ac247ae4d7c60bfc859af96065d08df28b
Greg Horn <gregmainland@gmail.com>
2013-05-23 16:27:28 +0200

    refactor MHE/MPC generateProtoGlue to use common configurations

--- rawesome_stuff/MHE.py
+++ rawesome_stuff/MHE.py
@@ -3,32 +3,48 @@ from rawe.ocp.Ocp import Mhe
 import casadi as C
 import carouselModel
 
-def makeMhe(dae,N,Ts,nSteps,iType,measNames,endMeasNames):
-    mhe = Mhe(dae, N=N, ts=Ts, measNames=measNames, endMeasNames=endMeasNames)
+from common_conf import Ts
 
-#    measurements = C.veccat( [dae[n] for n in ['marker positions','IMU acceleration','IMU angular velocity']])
-#    measurements = C.veccat( [measurements,
-#                              dae['cos_delta'],
-#                              dae['aileron'],
-#                              dae['elevator'],
-#                              dae['motor_torque'],
-#                              dae['daileron'],
-#                              dae['delevator']])
-#
-#    mhe.minimizeLsq(measurements)
-#    mhe.minimizeLsqEndTerm(measurements)
+mheHorizonN = 10
 
-#    xref = C.veccat( [mhe[n] for n in dae.xNames()])
-#    uref = C.veccat( [mhe[n] for n in dae.uNames()])
-#    yref  = C.veccat( [mhe[n] for n in measurements])
-#    yNref = C.veccat( [mhe[n] for n in measurementsEND])
+mheIntOpts = rawe.RtIntegratorOptions()
+mheIntOpts['INTEGRATOR_TYPE'] = 'INT_IRK_GL2'
+#mheIntOpts['INTEGRATOR_TYPE'] = 'INT_IRK_RIIA3'
+mheIntOpts['NUM_INTEGRATOR_STEPS'] = 4
+mheIntOpts['IMPLICIT_INTEGRATOR_NUM_ITS'] = 3
+mheIntOpts['IMPLICIT_INTEGRATOR_NUM_ITS_INIT'] = 0
+mheIntOpts['LINEAR_ALGEBRA_SOLVER'] = 'HOUSEHOLDER_QR'
+mheIntOpts['UNROLL_LINEAR_SOLVER'] = False
+mheIntOpts['IMPLICIT_INTEGRATOR_MODE'] = 'IFTR'
 
-#    dae['measurements'] = C.veccat([xref,uref])
-#    dae['measurementsN'] = xref
+mheOpts = rawe.OcpExportOptions()
+mheOpts['HESSIAN_APPROXIMATION'] = 'GAUSS_NEWTON'
+mheOpts['DISCRETIZATION_TYPE'] = 'MULTIPLE_SHOOTING'
+mheOpts['QP_SOLVER'] = 'QP_QPOASES'
+mheOpts['HOTSTART_QP'] = True
+mheOpts['SPARSE_QP_SOLUTION'] = 'CONDENSING'
+#mheOpts['SPARSE_QP_SOLUTION'] = 'FULL_CONDENSING_U2'
+#mheOpts['MAX_NUM_QP_ITERATIONS'] = '30'
+mheOpts['FIX_INITIAL_STATE'] = False
+#mheOpts['CG_USE_VARIABLE_WEIGHTING_MATRIX'] = False
+#mheOpts['CG_USE_C99'] = True
 
-#    mhe.minimizeLsq(yref)
-#    mhe.minimizeLsqEndTerm(yNref)
-    
+# normal measurements
+measNames  = ['marker_positions','IMU_angular_velocity','IMU_acceleration']
+measNames += ['r','cos_delta','sin_delta','aileron','elevator']
+measNames += ['daileron', 'delevator', 'dmotor_torque', 'dddr']
+
+endMeasNames  = ['IMU_angular_velocity']
+endMeasNames += ['r','cos_delta','sin_delta','aileron','elevator']
+
+## full state feedback
+#endMeasNames = ['x', 'y', 'z', 'e11', 'e12', 'e13', 'e21', 'e22', 'e23', 'e31', 'e32', 'e33', 'dx', 'dy', 'dz', 'w1', 'w2', 'w3', 'ddelta', 'r', 'dr', 'aileron', 'elevator', 'motor_torque', 'ddr', 'cos_delta', 'sin_delta']
+#measNames = endMeasNames + ['daileron', 'delevator', 'dmotor_torque', 'dddr']
+
+
+def makeMhe(dae):
+    mhe = Mhe(dae, N=mheHorizonN, ts=Ts, measNames=measNames, endMeasNames=endMeasNames)
+
     mhe.constrain(mhe['ConstR1'],'==',0, when='AT_END')
     mhe.constrain(mhe['ConstR2'],'==',0, when='AT_END')
     mhe.constrain(mhe['ConstR3'],'==',0, when='AT_END')
@@ -41,39 +57,11 @@ def makeMhe(dae,N,Ts,nSteps,iType,measNames,endMeasNames):
 
     mhe.constrain(mhe['ConstDelta'],'==',0, when='AT_END')
     
-    intOpts = rawe.RtIntegratorOptions()
-    intOpts['INTEGRATOR_TYPE'] = iType
-    intOpts['NUM_INTEGRATOR_STEPS'] = nSteps
-    intOpts['IMPLICIT_INTEGRATOR_NUM_ITS'] = 3
-    intOpts['IMPLICIT_INTEGRATOR_NUM_ITS_INIT'] = 0
-    intOpts['LINEAR_ALGEBRA_SOLVER'] = 'HOUSEHOLDER_QR'
-    intOpts['UNROLL_LINEAR_SOLVER'] = False
-    intOpts['IMPLICIT_INTEGRATOR_MODE'] = 'IFTR'
-    
-    ocpOpts = rawe.OcpExportOptions()
-    ocpOpts['HESSIAN_APPROXIMATION'] = 'GAUSS_NEWTON'
-    ocpOpts['DISCRETIZATION_TYPE'] = 'MULTIPLE_SHOOTING'
-    ocpOpts['QP_SOLVER'] = 'QP_QPOASES'
-    ocpOpts['HOTSTART_QP'] = True
-    ocpOpts['SPARSE_QP_SOLUTION'] = 'CONDENSING'
-#   ocpOpts['SPARSE_QP_SOLUTION'] = 'FULL_CONDENSING_U2'
-#   ocpOpts['MAX_NUM_QP_ITERATIONS'] = '30'
-    ocpOpts['FIX_INITIAL_STATE'] = False
-#    ocpOpts['CG_USE_VARIABLE_WEIGHTING_MATRIX'] = False
-#    ocpOpts['CG_USE_C99'] = True
-
 #    cgOpts = {'CXX':'g++', 'CC':'gcc'}
     cgOpts = {'CXX':'clang++', 'CC':'clang'}
-    mheRT = mhe.exportCode(codegenOptions=cgOpts,ocpOptions=ocpOpts,integratorOptions=intOpts)
+    mheRT = mhe.exportCode(codegenOptions=cgOpts,ocpOptions=mheOpts,integratorOptions=mheIntOpts)
     
 #    RintMeas = rawe.RtIntegrator(dae,ts=Ts, options=intOpts, measurements=measurements)
 #    RintMeasEnd = rawe.RtIntegrator(dae,ts=Ts, options=intOpts, measurements=measurementsEND)
     
     return mheRT
-
-
-if __name__=='__main__':
-    from highwind_carousel_conf import conf
-    dae = rawe.models.carousel(conf)
-    dae = carouselModel.makeModel(dae,conf)
-    OcpRt = makeMhe(dae,10,0.1)
--- rawesome_stuff/generateProtoGlue.py
+++ rawesome_stuff/generateProtoGlue.py
@@ -4,12 +4,17 @@ import sys
 from highwind_carousel_conf import conf
 import carouselModel
 
+import MHE
+import NMPC
+
 if __name__=='__main__':
     assert len(sys.argv) == 2, 'need to call generateProtoGlue.py with the properties directory'
     dae = carouselModel.makeModel(conf,propertiesDir=sys.argv[1])
 
     autogenDir = '.'
     topname = 'Carousel'
-    mheHorizN = 10
-    mpcHorizN = 10
-    rawe.utils.mkprotobufs.writeAll(dae, topname, autogenDir, mheHorizN=10, mpcHorizN=10)
+    rawe.utils.mkprotobufs.writeAll(dae, topname, autogenDir,
+                                    mheHorizN=MHE.mheHorizonN,
+                                    mpcHorizN=NMPC.mpcHorizonN,
+                                    measurements=MHE.measNames,
+                                    measurementsEnd=MHE.endMeasNames)
