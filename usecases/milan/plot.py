f924769db55f3bb777c131643b984ab07941bc91
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-06-25 16:35:21 +0200

    Updated calibration

--- usecases/milan/plot.py
+++ usecases/milan/plot.py
@@ -7,15 +7,19 @@ from pylab import plot,xlabel,ylabel,show,legend
 t = array(f.variables['lineAngleSensor.timeStamps.0'].data) # ns
 t = t*1e-6 # ms
 
+# Note, I manually calibrate the tether angle sensor
+# such that 0V in both angles corresponds to the tether
+# straight out.
+
 # Horizontal, positive is forward
 hslope = 0.4246 # V / degrees
-hzero = -0.1227 # volts
+hzero = 0.0 # volts
 v1 = array(f.variables['lineAngleSensor.voltages.0'].data)
 v1 = (v1-hzero)/hslope
 
 # Vertical, positive is up
 vslope = 0.6074 # V / degrees 
-vzero = 5.1781 # V
+vzero = 0.0 # V
 v2 = array(f.variables['lineAngleSensor.voltages.1'].data)
 v2 = (v2-vzero)/vslope
 
