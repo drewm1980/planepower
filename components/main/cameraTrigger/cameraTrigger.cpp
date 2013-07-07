2fc8a181ea5fd9ff08e972b2075a6d24c457cc4f
Milan Vukov <mvukov@gmail.com>
2013-07-07 12:04:45 +0200

    Cleaned the camera trigger component

--- components/main/cameraTrigger/cameraTrigger.cpp
+++ components/main/cameraTrigger/cameraTrigger.cpp
@@ -1,41 +1,61 @@
-#include <stdint.h>
-#include <ocl/Component.hpp>
-
 #include "cameraTrigger.hpp"
+
+#include <rtt/Logger.hpp>
+#include <rtt/Property.hpp>
+#include <rtt/os/TimeService.hpp>
+#include <rtt/Time.hpp>
 
 using namespace std;
 using namespace RTT;
-using namespace Orocos;
-using namespace BFL;
+using namespace RTT::os;
 
-ORO_CREATE_COMPONENT(CameraTrigger);
+#define TRIGGER_ACTIVE_HIGH 0
 
+#define SLEEP_TIME_MILLISECONDS 10
 
-#define TIME_TYPE uint64_t
-
-CameraTrigger::CameraTrigger(std::string name):TaskContext(name,PreOperational)
+CameraTrigger::CameraTrigger(std::string name)
+	: TaskContext(name, PreOperational)
 {
-	addEventPort("Trigger",_Trigger).doc("Trigger input/timestamp");
-	addPort("TriggerTriggeredTime",_TriggerTriggeredTime).doc("Timestamp when trigger was fired"); 
-	addPort("TriggerResetTime",_TriggerResetTime).doc("Timestamp when trigger was reset"); 
-	tempTime = RTT::os::TimeService::Instance()->getTicks(); // Get current time
+	TIME_TYPE tempTime;
+
+	addEventPort("Trigger", _Trigger)
+		.doc( "Trigger input/timestamp" );
+	addPort("TriggerTriggeredTime", _TriggerTriggeredTime)
+		.doc( "Timestamp when trigger was fired" ); 
+	addPort("TriggerResetTime", _TriggerResetTime)
+		.doc( "Timestamp when trigger was reset" );
+ 
+	tempTime = TimeService::Instance()->getTicks();
+
 	_TriggerTriggeredTime.setDataSample( tempTime );
 	_TriggerTriggeredTime.write( tempTime );
 	_TriggerResetTime.setDataSample( tempTime );
 	_TriggerResetTime.write( tempTime );
 }
 
-CameraTrigger::~CameraTrigger()
-{
-}
-
 bool  CameraTrigger::configureHook()
 {
-	// Configure a digital output pin on the EBox to use for our trigger
-	assert(this->hasPeer("soemMaster"));
-	assert(this->getPeer("soemMaster")->provides()->hasService("Slave_1001"));
-	assert(this->getPeer("soemMaster")->provides()->getService("Slave_1001")
-			->provides()->hasOperation("setBit"));
+	Logger::In in( getName() );
+
+	if (this->hasPeer("soemMaster") == false)
+	{
+		log( Error ) << "The is no soemMaster peer" << endlog();
+		goto configureHookFailed;
+	}
+
+	if (this->getPeer("soemMaster")->provides()->hasService("Slave_1001") == false)
+	{
+		log( Error ) << "The is no Slave_1001 service" << endlog();
+		goto configureHookFailed;
+	}
+
+	if (this->getPeer("soemMaster")->provides()->getService("Slave_1001")
+		->provides()->hasOperation("setBit") == false)
+	{
+		log( Error ) << "The is no operation setBit" << endlog();
+		goto configureHookFailed;
+	}
+
 	setBit = getPeer("soemMaster")->provides()->getService("Slave_1001")
 		->provides()->getOperation("setBit");
 
@@ -46,6 +66,9 @@ bool  CameraTrigger::configureHook()
 #endif
 
 	return true;
+
+configureHookFailed:
+	return false;
 }
 
 bool  CameraTrigger::startHook()
@@ -55,7 +78,9 @@ bool  CameraTrigger::startHook()
 
 void  CameraTrigger::updateHook()
 {
-	tempTime = RTT::os::TimeService::Instance()->getTicks();
+	TIME_TYPE tempTime;
+
+	tempTime = TimeService::Instance()->getTicks();
 	_TriggerTriggeredTime.write(tempTime);
 #if TRIGGER_ACTIVE_HIGH
 	pull_trigger_high();
@@ -66,26 +91,27 @@ void  CameraTrigger::updateHook()
 	usleep(SLEEP_TIME_MILLISECONDS*1e3);
 	pull_trigger_high();
 #endif
-	tempTime = RTT::os::TimeService::Instance()->getTicks();
+	tempTime = TimeService::Instance()->getTicks();
 	_TriggerResetTime.write(tempTime);
-
 }
 
 void  CameraTrigger::stopHook()
-{
-}
+{}
 
-void  CameraTrigger::cleanUpHook()
-{
-}
+void  CameraTrigger::cleanupHook()
+{}
+
+void  CameraTrigger::errorHook()
+{}
 
 void CameraTrigger::pull_trigger_high()
 {
-	setBit(0,1);
+	setBit(0, 1);
 }
 
 void CameraTrigger::pull_trigger_low()
 {
-	setBit(0,0);
+	setBit(0, 0);
 }
 
+ORO_CREATE_COMPONENT( CameraTrigger )
