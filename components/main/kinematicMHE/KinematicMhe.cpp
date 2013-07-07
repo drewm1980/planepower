d399c7fa2ca40a82feb71cb8667833a87923e425
Milan Vukov <mvukov@gmail.com>
2013-07-07 15:45:50 +0200

    Added kinematic MHE skeleton; very basic functionality for data acquisition testing. To be tested on the carousel1.

---
+++ components/main/kinematicMHE/KinematicMhe.cpp
@@ -0,0 +1,96 @@
+#include "KinematicMhe.hpp"
+
+#include <rtt/Logger.hpp>
+#include <rtt/Property.hpp>
+#include <rtt/os/TimeService.hpp>
+#include <rtt/Time.hpp>
+
+using namespace std;
+using namespace RTT;
+using namespace RTT::os;
+
+#define MAX_NUM_IMU_SAMPLES 100
+
+KinematicMhe::KinematicMhe(std::string name)
+	: TaskContext(name, PreOperational)
+//	  , portMcuHandlerData(ConnPolicy::buffer( MAX_NUM_IMU_SAMPLES ))
+{
+	addEventPort("trigger", portTrigger)
+		.doc("Trigger port");
+	addPort("mcuData", portMcuHandlerData)
+		.doc("MCU handler inputs");
+	addPort("encoderData",portEncoderData)
+		.doc("Encoder data");
+	addPort("ledTrackerData", portLEDTrackerData)
+		.doc("LED Tracker data");
+	addPort("debugData", portDebugData)
+		.doc("Debugging data");
+
+	portDebugData.setDataSample( debugData );
+	portDebugData.write( debugData );
+
+	imuData.resize( MAX_NUM_IMU_SAMPLES );
+}
+
+bool  KinematicMhe::configureHook()
+{
+	Logger::In in( getName() );
+
+	// TODO
+	if (portTrigger.connected() == false)
+	{
+		log( Error ) << "One of the input ports is not connected to a data source" << endlog();
+		goto configureHookFailed;
+	}
+
+	return true;
+
+configureHookFailed:
+	return false;
+}
+
+bool KinematicMhe::startHook()
+{
+	return true;
+}
+
+void KinematicMhe::updateHook()
+{
+	TIME_TYPE trigger;
+
+	debugData.ts_entry = TimeService::Instance()->getTicks();
+	
+	//
+	// Read the data ports
+	//
+	portTrigger.read( trigger );
+	
+	// It si assumed that this port will be buffered
+	unsigned numImuSamples = 0;
+	while(portMcuHandlerData.read( imuData[ numImuSamples ] ) == NewData)
+		numImuSamples++;
+
+	FlowStatus encStatus = portEncoderData.read( encData );
+	FlowStatus camStatus = portLEDTrackerData.read( camData );
+
+	// Very basic debugging
+	debugData.num_imu_samples = numImuSamples;
+	debugData.num_enc_samples = (encStatus == NewData);
+	debugData.num_cam_samples = (camStatus == NewData);
+
+	debugData.ts_trigger = trigger;
+	debugData.ts_elapsed = TimeService::Instance()->secondsSince( trigger );
+
+	portDebugData.write( debugData );
+}
+
+void KinematicMhe::stopHook()
+{}
+
+void KinematicMhe::cleanupHook()
+{}
+
+void KinematicMhe::errorHook()
+{}
+
+ORO_CREATE_COMPONENT( KinematicMhe )
---
+++ components/main/kinematicMHE/types/KinematicMheTypekit.hpp
@@ -0,0 +1,48 @@
+#ifndef __KINEMATIC_MHE_TYPEKIT__
+#define __KINEMATIC_MHE_TYPEKIT__
+
+#include "KinematicMheDataType.hpp"
+#include <rtt/types/StructTypeInfo.hpp>
+#include <rtt/types/TypekitPlugin.hpp>
+
+namespace boost
+{
+	namespace serialization
+	{
+		template< class Archive >
+		void serialize(Archive& a, KinematicMheDataType& l, unsigned int)
+		{
+			using boost::serialization::make_nvp;
+			
+			a & make_nvp("num_imu_samples", l.num_imu_samples);
+			a & make_nvp("num_enc_samples", l.num_enc_samples);
+			a & make_nvp("num_cam_samples", l.num_cam_samples);
+			
+			a & make_nvp("ts_trigger", l.ts_trigger);
+			a & make_nvp("ts_entry", l.ts_entry);
+			a & make_nvp("ts_elapsed", l.ts_elapsed);
+		}
+	}
+}
+
+struct KinematicMheDataTypeInfo
+	: public RTT::types::StructTypeInfo< KinematicMheDataType >
+{
+	KinematicMheDataTypeInfo()
+		: RTT::types::StructTypeInfo< KinematicMheDataType >( "KinematicMheData" )
+	{}
+};
+
+/// KinematicMhe typekit plugin
+class KinematicMheTypekit
+	: public RTT::types::TypekitPlugin
+{
+public:
+	virtual std::string getName();
+
+	virtual bool loadTypes();
+	virtual bool loadConstructors();
+	virtual bool loadOperators();
+};
+
+#endif // __KINEMATIC_MHE_TYPEKIT__
