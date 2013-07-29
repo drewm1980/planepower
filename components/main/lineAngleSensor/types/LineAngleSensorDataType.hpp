77e3c4e46fdbbc131b1ee02ab32786137ba39aed
Milan Vukov <mvukov@gmail.com>
2013-07-29 14:53:33 +0200

    Updated LAS typekit; added averaging function.

--- components/main/lineAngleSensor/types/LineAngleSensorDataType.hpp
+++ components/main/lineAngleSensor/types/LineAngleSensorDataType.hpp
@@ -1,6 +1,8 @@
 #ifndef __LINEANGLESENSORDATATYPE__
 #define __LINEANGLESENSORDATATYPE__
 
+#include <vector>
+
 /// Custom data type for the encoder component
 typedef struct
 {
@@ -9,5 +11,24 @@ typedef struct
 	/// Time-stamps: trigger [ticks] and elapsed [sec]
 	double ts_trigger, ts_elapsed;
 } LineAngleSensorDataType;
+
+/// Averaging function
+inline void average(std::vector< LineAngleSensorDataType >& vec, LineAngleSensorDataType& avg, unsigned size)
+{
+	unsigned sz = size < vec.size() ? size : vec.size();
+
+	avg.angle_hor = avg.angle_ver = 0.0;
+
+	for (unsigned i = 0; i < sz; ++i)
+	{
+		avg.angle_hor += vec[ i ].angle_hor;
+		avg.angle_ver += vec[ i ].angle_ver;
+	}
+	avg.angle_hor /= (float)sz;
+	avg.angle_ver /= (float)sz;
+
+	avg.ts_trigger = vec[sz - 1].ts_trigger;
+	avg.ts_elapsed = vec[sz - 1].ts_elapsed;
+}
 
 #endif // __LINEANGLESENSORDATATYPE__
