3b28448e5c1a6234639a049b8274acaa7bde3e8a
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-10-09 13:02:03 +0200

    Cleaned up Drive communication code UNTESTED

--- components/carousel2/wouter/main_client.cpp
+++ components/carousel2/wouter/main_client.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
 
 #include "udp_communication.hpp"
 #include <stdlib.h> 
@@ -14,33 +15,59 @@ void quit(){
 	exit(1);
 }
 
+// Returns speed as a signed integer in the format the PLC and drives want,
+// up to endianness
 int32_t getSpeedFromUser(){
 	printf("Please enter the speed as a percentage of the nominal speed:\n");
-	float speedpercent;
+	double speedpercent;
 	scanf("%f",&speedpercent);
-	const int nominalSpeed = 1073741824;
+	const int nominalSpeed = 0x40000000;
 	if (speedpercent>100.0) speedpercent=100.0;
 	if (speedpercent<-100.0) speedpercent=-100.0;
 	int32_t n = speedpercent/100.0*nominalSpeed;
 	return n;
 }
 
+// This is a wire format we defined to hold the references for both motors
+struct UDPSpeedCommand {
+	uint32_t winchSpeedReference;
+	uint32_t carouselSpeedReference;
+	uint32_t checksum;
+};
+
+// Compute checksum for our message type.
+// Not, the message must contain only uint32_t for this to work!!!
+void recompute_checksum(UDPSpeedCommand *c)
+{
+	int numentries = sizeof(UDPSpeedCommand)/sizeof(uint32_t)-1;
+	uint32_t sum = 0;
+	for(int i=0; i<numentries; i++)
+	{
+		sum += *((uint32_t *) c + i);
+		c->checksum = sum;
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
+	UDPSpeedCommand c;
+	memset(&c,sizeof(c),0);
 	int port_number = 2000;
 	char ip_address[] = "192.168.0.1";
 	printf("Opening client\n");
 	openUDPClientSocket(&udp_client,ip_address,port_number);
 	bool cont=1;
 	while(cont){
-		int32_t n = getSpeedFromUser();
-		int32_t n_swapped = __builtin_bswap32(n);
+		c.winchSpeedReference = __builtin_bswap32(getSpeedFromUser());
+		c.carouselSpeedReference = __builtin_bswap32(getSpeedFromUser());
+		recompute_checksum(&c);
 
-		if(sendUDPClientData(&udp_client, &n_swapped, sizeof(n_swapped)))
+		if(sendUDPClientData(&udp_client, &c, sizeof(c)))
 			printf("sending of data failed\n");
 		printf("Continue?\n");
-		char c = getchar();
-		if(c!='y' || c!='Y') cont=0;
+		char cc = getchar();
+		if(cc!='y' || cc!='Y') cont=0;
 	}
 	printf("Closing client\n");
 	closeUDPClientSocket(&udp_client);
