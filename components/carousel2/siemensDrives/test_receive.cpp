ea355b2524d82580ebccad43c446034756afca2d
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-10-10 16:42:06 +0200

    Add printing of siemens timing information

--- components/carousel2/siemensDrives/test_receive.cpp
+++ components/carousel2/siemensDrives/test_receive.cpp
@@ -14,16 +14,18 @@ int main(int argc, char *argv[])
 	SiemensCommunicator s;
 	SiemensDriveState ds;
 
-	cout << "Measuring how fast we are receiving packets from the PLC" << endl;
+	cout << "Waiting for at least one packet..." << endl;
+	s.read(&ds);
+
+	cout << "Measuring how fast we are receiving packets from the PLC..." << endl;
 	timespec t1, t2;
-	clock_gettime(CLOCK_REALTIME, &t1); // Works on Linux
+	clock_gettime(CLOCK_REALTIME, &t1); 
 	for(int i=0; i<1000; i++) { s.read(&ds); }
-	clock_gettime(CLOCK_REALTIME, &t2); // Works on Linux
+	clock_gettime(CLOCK_REALTIME, &t2); 
 	time_t dsec = t2.tv_sec-t1.tv_sec;
 	long dnsec = t2.tv_nsec-t1.tv_nsec;
-	
-	double dt;
-	//if(dnsec>0) dt = dsec + dnsec*1e-9
+	double dt = (dsec + dnsec*1.0e-9)/1000.0; // seconds per sample
+	cout << "Received 1000 packets at dt= " << dt/1000 << " ms per sample" << endl;
 
 	while(1)
 	{
