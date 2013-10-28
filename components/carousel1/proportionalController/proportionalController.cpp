d24e7c23a21a2c5cee7fd1289669e7d80631c760
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-10-28 12:49:46 +0100

    Still working, committing again cause last commit broke the build

--- components/carousel1/proportionalController/proportionalController.cpp
+++ components/carousel1/proportionalController/proportionalController.cpp
@@ -10,11 +10,24 @@ using namespace RTT::os;
 
 ProportionalController::ProportionalController(std::string name):TaskContext(name,PreOperational) 
 {
+	// Set up Input ports
+	addEventPort("lasData", portLASData)
+		.doc("Line angle sensor data in Radians");
+	addPort("encoderData",portEncoderData)
+		.doc("Encoder data");
+
+	// Set up Output port
 	addPort("controls", portControls)
 		.doc("Control values");
 	controls.resize(3, 0.0);
 	portControls.setDataSample( controls );
 	portControls.write( controls );
+	
+	// Properties
+	addProperty("azimuthElevator",   azimuthElevator);
+	addProperty("elevationElevator", elevationElevator);
+	addProperty("azimuthAileron",    azimuthAileron);
+	addProperty("elevationAileron",  elevationAileron);
 }
 
 bool ProportionalController::configureHook()
@@ -29,6 +42,15 @@ bool  ProportionalController::startHook()
 
 void  ProportionalController::updateHook()
 {
+	portLASData.read(lasData);
+	//portEncoderData.read(encoderData);
+	float az = lasData.angle_hor;
+	float el = lasData.angle_ver;
+	controls[0] = 0.0;
+	controls[1] = 0.0;
+	//controls[0] += 
+	controls[2] = controls[1];
+	portControls.write(controls);
 }
 
 void  ProportionalController::stopHook()
