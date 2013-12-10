0fc5ea0850c201efd6ea79e026eaadd69e9c0899
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-12-10 16:28:01 +0100

    Made function out of setting up reporters and moved it so helpers

--- lua/deployment_helpers.lua
+++ lua/deployment_helpers.lua
@@ -30,5 +30,34 @@ function P.get_property(instanceName, propertyName, propertyValue)
 	return _G[instanceName]:getProperty(propertyName):get(propertyValue)
 end
 
+function P.set_up_reporters(reporterBaseNames,reportedComponentNames)
+	reporterNames={}
+	reporterFileNames={}
+	for i=1,#reporterBaseNames do 
+		reporterNames[i]=reporterBaseNames[i].."Reporter"
+		reporterFileNames[i]=reporterBaseNames[i].."Data.nc"
+	end
+	rp = rtt.Variable("ConnPolicy")
+	rp.type = 2
+	rp.size = 4096
+	for i,reporterName in pairs(reporterNames) do
+		load_reporter(reporterName)
+		set_property(reporterName,"ReportFile",reporterFileNames[i])
+		set_property(reporterName,"ReportPolicy",rp)
+		set_property(reporterName,"ReportOnlyNewData",false)
+		componentName = reportedComponentNames[i]
+		deployer:connectPeers(reporterName,componentName)
+		r = _G[reporterName]
+		outputPortNames = get_output_ports(componentName)
+		for k,portName in ipairs(outputPortNames) do
+			r:reportPort(componentName, portName)
+			--print(reporterName.." is reporting component "..componentName.." port "..portName)
+		end
+		deployer:setActivity(reporterName,0.0,reporterPrio,ORO_SCHED_RT)
+		r:configure()
+		r:start()
+	end
+end
+
 return deployment_helpers
 
