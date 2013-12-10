fcffcbbaae9da37727534cda238c7ee0a065fde2
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-12-10 10:50:57 +0100

    Update orocos bootstrap script

--- extern/bootstrap_orocos.sh
+++ extern/bootstrap_orocos.sh
@@ -15,15 +15,43 @@ echo "Giving special rights to OROCOS deployer..."
 sh setcap.sh
 
 echo "Bootstrapping rttlua tab completion..."
-git clone git://gitorious.org/orocos-toolchain/rttlua_completion.git
+if [ ! -d rttlua_completion ]; then
+	git clone git://gitorious.org/orocos-toolchain/rttlua_completion.git
+else
+	(
+	cd rttlua_completion
+	git pull origin master
+	)
+fi
 (
 cd rttlua_completion
 make
 )
 
 echo "Bootstrapping rFSM..."
-git clone git@github.com:kmarkus/rFSM
+if [ ! -d rFSM ]; then
+	git clone git@github.com:kmarkus/rFSM
+else
+	(
+	cd rFSM
+	git pull origin master
+	)
+fi
 (
 cd rFSM
+make
+)
+
+echo "Bootstrapping dot service..."
+if [ ! -d rtt_dot_service ]; then
+	git clone git@gitorious.org:rtt_dot_service/rtt_dot_service.git
+else
+	(
+	cd rtt_dot_service
+	git pull origin master
+	)
+fi
+(
+cd rtt_dot_service
 make
 )
