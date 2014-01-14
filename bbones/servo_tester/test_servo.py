b03b70cf48102124bd3d7a7c3addecb50cf46993
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-01-14 17:53:52 +0100

    Fix bug causing insufficient rotation

--- bbones/servo_tester/test_servo.py
+++ bbones/servo_tester/test_servo.py
@@ -16,14 +16,14 @@ duty_ccw = pwm_ccw * freq * .001
 duty_center = pwm_center * freq * .001
 duty_cw = pwm_cw * freq * .001
 
-PWM.start("P8_13", duty_center*100.0, freq)
+PWM.start("P8_13", duty_center*100.0*fucked_up_bbone_clock_factor, freq)
 
 wave_period = 1
 
 while True:
-    PWM.set_duty_cycle("P8_13", duty_ccw*100.0)
+    PWM.set_duty_cycle("P8_13", duty_ccw*100.0*fucked_up_bbone_clock_factor)
     sleep(wave_period * 0.5)
-    PWM.set_duty_cycle("P8_13", duty_cw*100.0)
+    PWM.set_duty_cycle("P8_13", duty_cw*100.0*fucked_up_bbone_clock_factor)
     sleep(wave_period * 0.5)
 
 PWM.stop("P8_13")
