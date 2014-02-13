421368073f8cd58a54c04ea0499f8e3d6785d56b
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-02-13 11:59:55 +0100

    Update paths to use orocos installed via ros packages

--- extern/bootstrap_orocos.sh
+++ extern/bootstrap_orocos.sh
@@ -1,15 +1,9 @@
 #! /usr/bin/env bash
 echo "Bootstrapping OROCOS..."
 
-mkdir -p orocos
-(
-cd orocos
-wget http://rock-robotics.org/stable/autoproj_bootstrap
-ruby autoproj_bootstrap $@ git git://gitorious.org/orocos-toolchain/build.git branch=toolchain-2.6 push_to=git@gitorious.org:orocos-toolchain/build.git
-. $PWD/env.sh
-autoproj update
-autoproj fast-build
-)
+sudo apt-get update
+sudo apt-get install ros-hydro-orocos-toolchain
+source /opt/ros/hydro/setup.bash
 
 echo "Giving special rights to OROCOS deployer..."
 sh setcap.sh
