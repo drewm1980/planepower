0e4c7ff60bfee35addb4a3a4e21555711c3969f7
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-05-07 19:13:17 +0200

    Cleaning up in bbones/server

--- bbones/server/runReadLog.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
-clear
-gcc read_log.c udp_communication.c data_decoding.c -o read_log -DDEBUG 
-
-
-./read_log 0 127.0.0.1 8888
-
--- bbones/server/runServer.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
-clear
-gcc main_server.c log.c udp_communication.c uart_communication.c data_decoding.c analyze.c -pthread -o main_server -DDEBUG=0 -DANALYZE=0
-
-
-./main_server 10.33.136.22 7777 8888 
