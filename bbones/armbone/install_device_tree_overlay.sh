348f7b2432540b9366dfcc97f673bfa3eee84c26
Andrew on Beaglebone <andrew.beaglebone>
2014-05-08 10:43:27 +0000

    Fix typo

--- bbones/armbone/install_device_tree_overlay.sh
+++ bbones/armbone/install_device_tree_overlay.sh
@@ -3,5 +3,5 @@ mkdir /mnt/boot
 mount /dev/mmcblk0p1 /mnt/boot
 echo "Please add something like the following to the end of the single line of /mnt/boot/uEnv.txt:"
 echo "capemgr.enable_partno=BB-SPI1-01"
-eche " or "
+echo " or "
 echo "capemgr.enable_partno=BB-Bitbanging"
