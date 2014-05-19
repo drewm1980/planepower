4b47a15ee66b261162e065ac1d3814582d84b3f2
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-05-19 13:54:31 +0000

    Tinkering in zmq in python

---
+++ bbones/network_benchmarking/bench_zmq_protobuf_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#  Hello World server
+
+import zmq
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+	print "Waiting for message from client!"
+	message = socket.recv()
+	print "Sending response!"
+	socket.send("World")
+}
+
