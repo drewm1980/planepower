58bf1915c04e1d7a065ca29c1e1fb9e1aa3a38c6
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-05-28 01:30:59 +0000

    modified the main_arm to use the fast bitbang_spi_reader

--- bbones/armbone/bitbang_spi.h
+++ bbones/armbone/bitbang_spi.h
@@ -1,4 +1,5 @@
 #include "stdint.h"
+#include "spi_communication.h"
 
 // API for reading from two aksim angle encoders to a beaglebone
 // using a bitbanged spi implementation
@@ -8,6 +9,8 @@
 void bitbang_init(); // Needs to be called before  read_angle_sensors to initialize uc pins, open device files
 // Returns already calibrated absolute angle values for the encoders
 void read_angle_sensors(float* azimuth_radians, float* elevation_radians);
+void read_angle_raw(uint16_t *rsp_azimuth, uint16_t *rsp_elevation);
+SPI_errCode spi_read_fast(uint8_t data_sensors[]);
 void bitbang_close();
 
 // This is just for writing fun debugging code
