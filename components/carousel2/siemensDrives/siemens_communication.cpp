9de6c881cf0533b9875cc3bfcc7e1fd47afedc6d
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-05-28 17:07:04 +0200

    Split the Siemens Communicator class in two so it can be two components

--- components/carousel2/siemensDrives/siemens_communication.cpp
+++ components/carousel2/siemensDrives/siemens_communication.cpp
@@ -7,29 +7,6 @@
 
 using namespace std;
 
-// Compute checksum for our message type.
-// Note, the message must contain only uint32_t for this to work!!!
-// Returns true if existing checksum was already correct
-// This should be called ~before byteswapping when sending
-//                   and ~after byteswapping when recieving
-template <class Packet>
-bool recompute_checksum(Packet *c)
-{
-	int numentries = sizeof(Packet)/sizeof(uint32_t)-1;
-	uint32_t sum = 0;
-	for(int i=0; i<numentries; i++)
-	{
-		sum += *((uint32_t *) c + i);
-	}
-	if(c->checksum == sum)
-	{
-		return true;
-	} else {
-		c->checksum = sum;
-		return false;
-	}
-}
-
 // bswap32 all of the entries of a stuct containing only (u)int32_t's
 // Not, the message must contain only uint32_t for this to work!!!
 template <class Packet>
@@ -46,36 +23,38 @@ void bswap_packet(Packet *c)
 	}
 }
 
-SiemensCommunicator::SiemensCommunicator()
+SiemensSender::SiemensSender()
 { 
 	strncpy(ip_address,"192.168.000.001",sizeof(ip_address)-1);
 	ip_address[15] = '\0';
 	port_number = 2000;
 	unsigned int timeout = 1000000; // us
 	openUDPClientSocket(&udp_client,ip_address,port_number,timeout);
-	openUDPServerSocket(&udp_server,port_number,timeout);
 	currentWinchCommand = 0;
 	currentCarouselCommand = 0;
 }
-SiemensCommunicator::~SiemensCommunicator()
+SiemensSender::~SiemensSender()
 {
 	send_reference_speeds(0,0);
 	closeUDPClientSocket(&udp_client);
-	closeUDPServerSocket(&udp_server);
 }
 
-int SiemensCommunicator::send_reference_speeds(double winchSpeed, double carouselSpeed)
+int SiemensSender::send_reference_speeds(double winchSpeed, double carouselSpeed)
 {
 	UDPSendPacket udpsc;
 	memset(&udpsc,0,sizeof(udpsc));
+	
+	// Clamp the values to the maxima, to prevent wraparound.
+	if(winchSpeed>maxWinchSpeed) winchSpeed = maxWinchSpeed;
+	if(winchSpeed<-maxWinchSpeed) winchSpeed = -maxWinchSpeed;
+	if(carouselSpeed>maxCarouselSpeed) carouselSpeed = maxCarouselSpeed;
+	if(carouselSpeed<-maxCarouselSpeed) carouselSpeed = -maxCarouselSpeed;
+
 	int32_t n1 = winchSpeed/nominalWinchSpeed*nominalCommand;
 	int32_t n2 = carouselSpeed/nominalCarouselSpeed*nominalCommand;
 	n2 = 0; // TEMPORARY SAFETY HACK
 	udpsc.winchSpeedReference = n1;
 	udpsc.carouselSpeedReference = n2;
-#if CHECKSUM
-	recompute_checksum(&udpsc);
-#endif
 	bswap_packet(&udpsc);
 
 	if(sendUDPClientData(&udp_client, &udpsc, sizeof(udpsc)))
@@ -89,32 +68,38 @@ int SiemensCommunicator::send_reference_speeds(double winchSpeed, double carouse
 	}
 }
 
-int SiemensCommunicator::send_winch_reference_speed(double winch_speed)
+int SiemensSender::send_winch_reference_speed(double winch_speed)
 {
 	return send_reference_speeds(winch_speed, currentCarouselCommand);
 }
 
-int SiemensCommunicator::send_carousel_reference_speed(double carousel_speed)
+int SiemensSender::send_carousel_reference_speed(double carousel_speed)
 {
 	return send_reference_speeds(currentWinchCommand, carousel_speed);
 }
 
+SiemensReceiver::SiemensReceiver()
+{ 
+	port_number = 2000;
+	unsigned int timeout = 1000000; // us
+	openUDPServerSocket(&udp_server,port_number,timeout);
+}
+SiemensReceiver::~SiemensReceiver()
+{
+	closeUDPServerSocket(&udp_server);
+}
+
 // Blocks until a UDP packet arrives!
-void SiemensCommunicator::read(SiemensDriveState* ds)
+void SiemensReceiver::read(SiemensDriveState* ds)
 {
 	UDPReceivePacket c;
 	memset(ds,0,sizeof(SiemensDriveState));
 	receiveUDPServerData(&udp_server,(void *)&c,sizeof(c)); //blocking !!!
 	bswap_packet(&c);
-#if CHECKSUM
-	bool success = recompute_checksum(&c);	
-	if(!success) printf("Warning, a UDP packet failed the checksum!!!\n");
-#endif
-	
 	ds->winchSpeedSmoothed = ((double) c.winchSpeedSmoothed)/(nominalCommand);
 }
 
-void SiemensCommunicator::handle_32bit_rollover(EncoderState *e, uint32_t smallCounts)
+void SiemensReceiver::handle_32bit_rollover(EncoderState *e, uint32_t smallCounts)
 {
 	const int64_t max_change = 0x3FFFFFFF;
 	int64_t diff = smallCounts - e->smallCountsLast;
--- components/carousel2/siemensDrives/siemens_communication.hpp
+++ components/carousel2/siemensDrives/siemens_communication.hpp
@@ -4,29 +4,27 @@
 #include "stdint.h"
 #include "udp_communication.h"
 
-#define CHECKSUM 0
-
 // This is a high-level API for interfacing with the siemens drives.
 // This API is NOT threadsafe, i.e. it should only ever be used from ONE thread!!
 
 // The following values must be configured to match in the siemens "Starter" app.
 #define nominalWinchShaftSpeed 3000.0 // RPM Nominal drive shaft rotation speed
+#define maxWinchShaftSpeed 1500.0 // RPM
 #define winchGearRatio 3.0  // input rotations per output rotation
 #define winchDrumRadius 0.20 // m
 #define PI 3.14159265359
 #define nominalWinchSpeed (nominalWinchShaftSpeed/winchGearRatio*2.0*PI/60*winchDrumRadius) // m/s The speed of the surface of the drum
+#define maxWinchSpeed (maxWinchShaftSpeed/winchGearRatio*2.0*PI/60*winchDrumRadius) // m/s The speed of the surface of the drum
 #define nominalCarouselShaftSpeed 1440.0 // RPM Nominal drive shaft rotation speed
 #define carouselGearRatio (14.68*1.5) // input rotations per output rotation.  Includes gears and belt.
-#define nominalCarouselSpeed (nominalCarouselShaftSpeed/carouselGearRatio*2.0*PI/60) // rad/s Nominal drive shaft rotation speed
+#define nominalCarouselSpeed (nominalCarouselShaftSpeed/carouselGearRatio*2.0*PI/60) // rad/s Nominal drive shaft rotation speed, a bit above 2*pi rad/s
+#define maxCarouselSpeed (2*PI) // rad/s
 #define nominalCommand 0x40000000 // This is the command corresponding to the nominal drive shaft rotation speed
 
 // This is a wire format we defined to hold the references for both motors
 struct UDPSendPacket {
 	uint32_t winchSpeedReference;
 	uint32_t carouselSpeedReference;
-#if CHECKSUM
-	uint32_t checksum;
-#endif
 };
 
 // This is a wire format defined in the PLC and starter software
@@ -41,9 +39,6 @@ struct UDPReceivePacket{
 	int32_t carouselTorque;
 	int32_t carouselPower;
 	int32_t carouselSpeedSetpoint;
-#if CHECKSUM
-	uint32_t checksum;
-#endif
 };
 
 // This is a unit converted version of UDPReceivePacket
@@ -66,16 +61,35 @@ struct EncoderState
 	int32_t smallCountsLast;
 };
 
-class SiemensCommunicator
+class SiemensSender
 {
 	public:
-		SiemensCommunicator();
-		~SiemensCommunicator();
-		int send_reference_speeds(double winch_speed, // in m/s, positive for reelout
+		SiemensSender();
+		~SiemensSender();
+		int send_reference_speeds(double winch_speed, // in percentage of nominal value
+				double carousel_speed); // in percentage of nominal value
+		int send_calibrated_speeds(double winch_speed, // in m/s, positive for reelout
 				double carousel_speed); // in rad/s, positive for clockwise rotation when viewed from above
 		int send_winch_reference_speed(double winch_speed);
 		int send_carousel_reference_speed(double carousel_speed);
 
+	private:
+
+		UDP udp_client;
+		int port_number;
+		char ip_address[16];
+		
+		double currentWinchCommand;
+		double currentCarouselCommand;
+
+};
+
+class SiemensReceiver
+{
+	public:
+		SiemensReceiver();
+		~SiemensReceiver();
+		
 		//// Read a UDP packet from the drives and perform unit conversions.
 		////	See the comments for the SiemensDriveState for more info.
 		//// Note:  While the motors are turning, this function should be
@@ -84,18 +98,12 @@ class SiemensCommunicator
 		void read(SiemensDriveState* ds);
 
 	private:
+		void handle_32bit_rollover(EncoderState *e, uint32_t smallCounts);
 		EncoderState winch;
 		EncoderState carousel;
-		void handle_32bit_rollover(EncoderState *e, uint32_t smallCounts);
-
-		UDP udp_client;
 		UDP udp_server;
 		int port_number;
 		char ip_address[16];
-		
-		double currentWinchCommand;
-		double currentCarouselCommand;
-
 };
 
 #endif
