65a3716763cec431ab74b94f3da0af6e48cdea1d
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-06-21 17:11:13 +0200

    Wrote initial version of resampler component

--- components/carousel2/resampler/resampler.cpp
+++ components/carousel2/resampler/resampler.cpp
@@ -8,6 +8,8 @@ using namespace std;
 using namespace RTT;
 using namespace RTT::os;
 
+typedef uint64_t TIME_TYPE;
+
 Resampler::Resampler(std::string name):TaskContext(name,PreOperational) 
 {
 	addPort("driveState",portDriveState).doc("Siemens Drives Measurements");
@@ -31,8 +33,24 @@ bool  Resampler::startHook()
 
 void  Resampler::updateHook()
 {
+	TIME_TYPE trigger = TimeService::Instance()->getTicks();
+
 	portDriveState.read(driveState);
 	portLineAngles.read(lineAngles);
+
+	// TODO: Actually keep a buffer of previous measurements,
+	// and implement a proper downsampling filter of appropriate length.
+	// For now, just send the most recent measurements from each sensor.
+	resampledMeasurements.winchSpeedSmoothed = driveState.winchSpeedSmoothed;
+	resampledMeasurements.winchEncoderPosition = driveState.winchEncoderPosition;
+	resampledMeasurements.carouselSpeedSmoothed = driveState.carouselSpeedSmoothed;
+	resampledMeasurements.carouselEncoderPosition = driveState.carouselEncoderPosition;
+
+	resampledMeasurements.azimuth = lineAngles.azimuth;
+	resampledMeasurements.elevation = lineAngles.elevation;
+
+	resampledMeasurements.ts_trigger = trigger;
+	resampledMeasurements.ts_elapsed = TimeService::Instance()->secondsSince( trigger );
 
 	portData.write(resampledMeasurements);
 }
