6fd26f180487584e9d66a2054b669c19f96cba56
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-06-25 13:42:36 +0200

    Add output port to lineAngleSensor2 component

--- components/carousel2/lineAngleSensor2/lineAngleSensor2.cpp
+++ components/carousel2/lineAngleSensor2/lineAngleSensor2.cpp
@@ -10,12 +10,16 @@ using namespace std;
 using namespace RTT;
 using namespace RTT::os;
 
+typedef uint64_t TIME_TYPE;
+
 LineAngleSensor2::LineAngleSensor2(std::string name):TaskContext(name,PreOperational) 
 {
+	addPort("data", portData).doc("The output data port");
 }
 
 bool LineAngleSensor2::configureHook()
 {
+	memset(&lineAngles, 0, sizeof( LineAngles ));
 	return true;
 }
 
@@ -29,9 +33,17 @@ bool  LineAngleSensor2::startHook()
 
 void  LineAngleSensor2::updateHook()
 {
-	// Do blocking wait for new line angle measurements here
 
-	usleep(500000); // make sure we don't burn spin the cpu in a high priority thread during testing.
+	TIME_TYPE trigger = TimeService::Instance()->getTicks();
+
+	// Do blocking wait for new line angle measurements here
+	usleep(50000); // make sure we don't burn spin the cpu in a high priority thread during testing.
+	//lineAngles.azimuth = -0.5 + 1.2 * sin(.2*3.1415 * trigger + 1.8);
+	//lineAngles.elevation = -0.4 + 0.7 * sin(.1*3.1415 * trigger + 1.8);
+
+	lineAngles.ts_trigger = trigger;
+	lineAngles.ts_elapsed = TimeService::Instance()->secondsSince( trigger );
+	portData.write(lineAngles);
 
 	// Re-trigger this comonent internally
 	if(keepRunning) this->getActivity()->trigger(); 
