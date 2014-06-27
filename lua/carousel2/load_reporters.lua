0ef1f319ab659b809d153b974c74866e35748778
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-06-27 10:52:38 +0200

    Repair plot_timing_info when .nc files missing

--- lua/carousel2/load_reporters.lua
+++ lua/carousel2/load_reporters.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env rttlua-i
 
+require "os"
+
 -- Return a list of strings containing just the names of the output
 -- ports of a component
 require "deployment_helpers"
@@ -10,7 +12,6 @@ end
 -- NOTE!!!  MAKE SURE THE DIRECTORIES THAT CONTAIN THE "types" DIRECTORIES
 -- ARE ON YOUR RTT_COMPONENT_PATH environment variable, otherwise only one
 -- typekit will be loaded at random (?), and you will be confused as fuck.
-
 -- The typekits will still work in the components for some reason, but
 -- the reporters will not start!
 
@@ -20,6 +21,9 @@ reportedComponentNames={"siemensSensors",
 						--"gainLoader", -- sent more frequently from controller
 						"controller",
 						"functionGenerator"}
+
+-- Blow away the existing *.nc files to avoid confusion
+os.execute("rm *.nc")
 
 reporterBaseNames=reportedComponentNames -- shallow copy
 set_up_reporters(reporterBaseNames,reportedComponentNames)
