c1c83f14f68320800f75dffef3fcc6bb0f9c204f
Lukas Klar <lukas.klar@mars.uni-freiburg.de>
2014-07-10 15:24:29 +0200

    modified rampGenerator so it does what it is supposed to do. Extracted all experiment function from main.lua to experiment_helpers.lua.

--- lua/carousel2/run_experiment.lua
+++ lua/carousel2/run_experiment.lua
@@ -1,9 +1,12 @@
 #!/usr/bin/env rttlua-i
 dofile("main.lua") 
 
-which_experiment = 6
+which_experiment = 0
 
-if which_experiment == 1 then
+if which_experiment == 0 then
+	print "No experiment selected!"
+	print "You are in 'free lua mode'. If you don't exactly know what you are doing close it! (Ctrl + D)"
+elseif which_experiment == 1 then
 	run()
 elseif which_experiment == 2 then
 	run_step_experiment()
@@ -15,8 +18,10 @@ elseif which_experiment == 5 then
 	run_slow_ramp_to_max_and_back_to_0_experiment()
 elseif which_experiment == 6 then
 	run_very_slow_ramp_to_max_and_back_to_0_experiment()
+elseif which_experiment == 7 then
+	run_rampGenerator_test()
 else 
 	print "No experiment selected!"
-	os.exit()
 	print "Exiting"
-end
+	os.exit()
+
