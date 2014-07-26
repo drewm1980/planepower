8979c491824092cf902d4d2b2a0ca20d188795a8
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-07-26 16:30:09 +0200

    Fix up shell script

--- cletus_vis/run_hprotoc.sh
+++ cletus_vis/run_hprotoc.sh
@@ -1,4 +1,6 @@
-find ~/planepower -iname '*.proto' | xargs cp proto_defs/
+#!/usr/bin/env bash
+proto_defs=`find ../components/carousel2 -iname '*.proto'`
 
-for file in proto_defs:
+for file in proto_defs; do
     hprotoc --haskell_out=src -p Protos proto_defs/$(file)
+done
